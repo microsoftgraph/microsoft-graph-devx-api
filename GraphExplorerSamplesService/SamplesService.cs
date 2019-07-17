6f9fe60042ab5cca2dff4a59d3d368a32a433ef7
Irvine Sunday <v-irsund@microsoft.com>
2019-07-17 10:24:48 +0300

    Adding samples controller and library service

---
+++ GraphExplorerSamplesService/SamplesService.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace GraphExplorerSamplesService
+{
+    public class SampleService
+    {
+    }
+}
---
+++ GraphWebApi/Controllers/GraphExplorerSamplesController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GraphWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GraphExplorerSamplesController : ControllerBase
+    {
+        [HttpGet]
+        [Produces("application/json")]
+        public IActionResult Get()
+        {
+            return View();
+        }
+
+        //public async Task<IActionResult> PostAsync()
+        //{
+
+        //}
+    }
+}
