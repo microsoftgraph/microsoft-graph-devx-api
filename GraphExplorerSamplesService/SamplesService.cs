79638c3a77d8759f070912e8acdfc603b518785e
Irvine Sunday <v-irsund@microsoft.com>
2019-07-26 15:50:11 +0300

    Single Responsibility refactoring
    Refactoring out the SamplesService so that it focuses on one core functionality - serialization and deserialization from and to sample query objects

--- GraphExplorerSamplesService/SamplesService.cs
+++ GraphExplorerSamplesService/SamplesService.cs
@@ -4,36 +4,30 @@ using System.IO;
 
 namespace GraphExplorerSamplesService
 {
-    public class SamplesService : ISamplesService
+    public class SamplesService
     {
         /// <summary>
-        /// Reads from a provided JSON file.
+        /// Deserializes a JSON string into a list of sample query objects.
         /// </summary>
-        /// <param name="filePathName">The directory path name of the JSON file.</param>
-        /// <exception cref="FileNotFoundException">Thrown when the provided file path name is unable to be resolved.</exception>
-        /// <returns>An enumerable collection of all the Graph Explorer sample queries in the provided JSON file.</returns>
-        public SampleQueriesList ReadFromJsonFile(string filePathName)
+        /// <param name="jsonString">The JSON string to be deserialized into a list of sample query objects.</param>
+        /// <exception cref="JsonException">Thrown when an error occurs during deserialization.</exception>
+        /// <exception cref="Exception">Thrown when any other error occurs during deserialization.</exception>
+        /// <returns>The deserialized list of sample query objects.</returns>
+        public static SampleQueriesList GetSampleQueriesList(string jsonString)
         {
             try
             {
-                if (File.Exists(filePathName))
-                {
-                    using (StreamReader streamReader = new StreamReader(filePathName))
-                    {
-                        string samplesQueriesString = streamReader.ReadToEnd();
-                        SampleQueriesList sampleQueriesList = JsonConvert.DeserializeObject<SampleQueriesList>(samplesQueriesString);
-                        return sampleQueriesList;
-                    }
-                }
-                else
-                {
-                    throw new FileNotFoundException("File not found!", filePathName);
-                }
+                SampleQueriesList sampleQueriesList = JsonConvert.DeserializeObject<SampleQueriesList>(jsonString);
+                return sampleQueriesList;
+            }
+            catch (JsonException jsonException)
+            {
+                throw jsonException;
             }
             catch (Exception exception)
             {
                 throw exception;
-            }            
+            }
         }
     }
 }
