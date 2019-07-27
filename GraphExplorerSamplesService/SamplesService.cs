ac6c166e876992aca52662ed91a57737bc48a53b
Irvine Sunday <v-irsund@microsoft.com>
2019-07-28 02:53:01 +0300

    Adding exceptions to SamplesService

--- GraphExplorerSamplesService/SamplesService.cs
+++ GraphExplorerSamplesService/SamplesService.cs
@@ -1,29 +1,41 @@
 ﻿using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace GraphExplorerSamplesService
 {
-    public class SamplesService
+    public static class SamplesService
     {
         /// <summary>
         /// Deserializes a JSON string into a list of sample query objects.
         /// </summary>
         /// <param name="jsonString">The JSON string to be deserialized into a list of sample query objects.</param>
+        /// <exception cref="ArgumentNullException">Thrown when the parameter is empty or null.</exception>
+        /// <exception cref="JsonReaderException">Thrown when an error occurs when reading the JSON string.</exception>
         /// <exception cref="JsonException">Thrown when an error occurs during deserialization.</exception>
         /// <exception cref="Exception">Thrown when any other error occurs during deserialization.</exception>
         /// <returns>The deserialized list of sample query objects.</returns>
         public static SampleQueriesList GetSampleQueriesList(string jsonString)
         {
+            if(String.IsNullOrEmpty(jsonString))
+            {
+                throw new ArgumentNullException();
+            }
+
             try
             {
-                SampleQueriesList sampleQueriesList = JsonConvert.DeserializeObject<SampleQueriesList>(jsonString);
+                var sampleQueriesList = JsonConvert.DeserializeObject<SampleQueriesList>(jsonString);
                 return sampleQueriesList;
+            }
+            catch(JsonReaderException readerException)
+            {
+                throw readerException;
             }
             catch (JsonException jsonException)
             {
                 throw jsonException;
-            }
+            }            
             catch (Exception exception)
             {
                 throw exception;
