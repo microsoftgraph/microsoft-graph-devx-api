0d041ddab3f111aef067cd4dad2360254a4e7ccd
Irvine Sunday <v-irsund@microsoft.com>
2019-07-30 11:08:29 +0300

    Removing exceptions from utility classes
    This is to allow for exceptions to be propagated up the call chain and get caught by the calling code to preserve the stack trace.

--- GraphExplorerSamplesService/SamplesService.cs
+++ GraphExplorerSamplesService/SamplesService.cs
@@ -11,9 +11,6 @@ namespace GraphExplorerSamplesService
         /// Deserializes a JSON string into a list of sample query objects.
         /// </summary>
         /// <param name="jsonString">The JSON string to be deserialized into a list of sample query objects.</param>
-        /// <exception cref="ArgumentNullException">Thrown when the parameter is empty or null.</exception>
-        /// <exception cref="JsonReaderException">Thrown when an error occurs when reading the JSON string.</exception>
-        /// <exception cref="JsonException">Thrown when an error occurs during deserialization.</exception>
         /// <returns>The deserialized list of sample query objects.</returns>
         public static SampleQueriesList GetSampleQueriesList(string jsonString)
         {
@@ -22,19 +19,8 @@ namespace GraphExplorerSamplesService
                 throw new ArgumentNullException(nameof(jsonString), "The JSON string to be deserialized cannot be null.");
             }
 
-            try
-            {
-                var sampleQueriesList = JsonConvert.DeserializeObject<SampleQueriesList>(jsonString);
-                return sampleQueriesList;
-            }
-            catch(JsonReaderException readerException)
-            {
-                throw readerException;
-            }
-            catch (JsonException jsonException)
-            {
-                throw jsonException;
-            } 
+            var sampleQueriesList = JsonConvert.DeserializeObject<SampleQueriesList>(jsonString);
+            return sampleQueriesList;
         }
     }
 }
