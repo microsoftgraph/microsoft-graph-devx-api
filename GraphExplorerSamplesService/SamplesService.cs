1da55210db153d34f8ba179f8e2c127b6bd6e0d5
Irvine Sunday <v-irsund@microsoft.com>
2019-07-31 17:47:09 +0300

    Adding new utility methods for SamplesService
    Update, Serialize and AddTo SampleQueriesList utility functions.

--- GraphExplorerSamplesService/SamplesService.cs
+++ GraphExplorerSamplesService/SamplesService.cs
@@ -22,5 +22,85 @@ namespace GraphExplorerSamplesService
             var sampleQueriesList = JsonConvert.DeserializeObject<SampleQueriesList>(jsonString);
             return sampleQueriesList;
         }
+
+        /// <summary>
+        /// Updates a list of sample queries with a sample query model object.
+        /// </summary>
+        /// <param name="sampleQueriesList">The samples queries list to be updated.</param>
+        /// <param name="sampleQueryModel">The sample query model object to update.</param>
+        /// <param name="sampleQueryId">The Id value of the sample query model object to be updated.</param>
+        /// <returns>The list of updated samples queries.</returns>
+        public static SampleQueriesList UpdateSampleQueriesList(SampleQueriesList sampleQueriesList, SampleQueryModel sampleQueryModel, Guid sampleQueryId)
+        {
+            if (sampleQueriesList == null)
+            {
+                throw new ArgumentNullException(nameof(sampleQueriesList), "The list of Sample Queries cannot be null.");
+            }
+            if (sampleQueryModel == null)
+            {
+                throw new ArgumentNullException(nameof(sampleQueryModel), "The Sample Query model object cannot be null.");
+            }
+
+            // Check if the sample query model exists in the sample queries list using its Id
+            int sampleQueryIndex = sampleQueriesList.SampleQueries.FindIndex(x => x.Id == sampleQueryId);
+            if (sampleQueryIndex >= 0)
+            {
+                // Insert the new sample query model into the sample queries list at the index of the original sample query model
+                sampleQueriesList.SampleQueries.Insert(sampleQueryIndex, sampleQueryModel);
+
+                // Remove the original sample query from the next index
+                sampleQueriesList.SampleQueries.RemoveAt(++sampleQueryIndex);
+
+                // Return the list 
+                return sampleQueriesList;
+            }
+            else // sample query model object is not in the sample queries list
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Serializes a list of samples queries into a JSON string.
+        /// </summary>
+        /// <param name="sampleQueriesList">The samples queries list to be deserialized.</param>
+        /// <returns>The serialized JSON string from a list of samples queries.</returns>
+        public static string SerializeSampleQueriesList (SampleQueriesList sampleQueriesList)
+        {
+            if (sampleQueriesList == null)
+            {
+                throw new ArgumentNullException(nameof(sampleQueriesList), "The list of Sample Queries cannot be null.");
+            }
+
+            var sampleQueriesJson = JsonConvert.SerializeObject(sampleQueriesList);
+            return sampleQueriesJson;
+        }
+
+        /// <summary>
+        /// Adds a sample query model object into a list of samples queries list.
+        /// </summary>
+        /// <param name="sampleQueriesList">The samples queries list which the sample query model object will be added into.</param>
+        /// <param name="sampleQueryModel">The sample query model object to be added.</param>
+        /// <returns>The list of samples queries with the newly added sample query model object.</returns>
+        public static SampleQueriesList AddToSampleQueriesList(SampleQueriesList sampleQueriesList, ref SampleQueryModel sampleQueryModel)
+        {
+            if (sampleQueriesList == null)
+            {
+                throw new ArgumentNullException(nameof(sampleQueriesList), "The list of Sample Queries cannot be null.");
+            }
+            if (sampleQueryModel == null)
+            {
+                throw new ArgumentNullException(nameof(sampleQueryModel), "The Sample Query model object cannot be null.");
+            }
+
+            // Append new GUID Id to the new sample query
+            sampleQueryModel.Id = Guid.NewGuid();
+
+            // Add the new sample query into the samples queries list
+            sampleQueriesList.SampleQueries.Add(sampleQueryModel);
+
+            // Return the samples queries list
+            return sampleQueriesList;
+        }
     }
 }
