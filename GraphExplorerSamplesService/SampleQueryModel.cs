9b1c335881ccfaa940e666618ef858555e9bb9b7
Irvine Sunday <v-irsund@microsoft.com>
2019-08-04 20:25:51 +0300

    Adding constructor and Categories property and their retrieval method
    Categories property sets the list of allowable categories. The constructor instantiates the Category property.

--- GraphExplorerSamplesService/SampleQueryModel.cs
+++ GraphExplorerSamplesService/SampleQueryModel.cs
@@ -2,7 +2,7 @@
 using Newtonsoft.Json.Converters;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
+using System.Text;
 
 namespace GraphExplorerSamplesService
 {
@@ -11,13 +11,34 @@ namespace GraphExplorerSamplesService
         // Field data for the custom mutator methods
         private string _humanName;
         private string _docLink;
+        private string _category;
+        private readonly LinkedList<string> _categories;
+
+        public SampleQueryModel()
+        {
+            _categories = new LinkedList<string>();
+            Categories = _categories;
+        }
+
 
         /* Properties */
 
         public Guid Id { get; set; }
 
         [JsonProperty(Required = Required.Always)]
-        public string Category { get; set; }
+        public string Category
+        {
+            get => _category;
+            set
+            {
+                if (!Categories.Contains(value))
+                {
+                    throw new ArgumentOutOfRangeException(nameof(Category), "The category specified does not exist in the list of categories:\r\n" + GetListOfCategories().ToString());
+                }
+
+                _category = value;
+            }
+        }
 
         [JsonProperty(Required = Required.Always)]
         [JsonConverter(typeof(StringEnumConverter))]
@@ -26,7 +47,7 @@ namespace GraphExplorerSamplesService
         [JsonProperty(Required = Required.Always)]                   
         public string HumanName
         {
-            get { return _humanName; }
+            get => _humanName;
             set
             {
                 if (value.Length > 64)
@@ -36,14 +57,14 @@ namespace GraphExplorerSamplesService
 
                 _humanName = value;
             }
-        } 
+        }
 
         [JsonProperty(Required = Required.Always)]
         public string RequestUrl { get; set; }
                 
         public string DocLink
         {
-            get { return _docLink; }
+            get => _docLink;
             set
             {
                 if (!Uri.IsWellFormedUriString(value, UriKind.Absolute))
@@ -63,6 +84,52 @@ namespace GraphExplorerSamplesService
 
         public bool SkipTest { get; set; }
 
+        [JsonIgnore]
+        public LinkedList<string> Categories
+        {
+            get => _categories;
+            private set
+            {
+                // The order is important
+                _categories.AddLast("Getting Started");
+                _categories.AddLast("Users");
+                _categories.AddLast("Groups");
+                _categories.AddLast("Outlook Mail");
+                _categories.AddLast("Outlook Mail (beta)");
+                _categories.AddLast("Outlook Calendar");
+                _categories.AddLast("Personal Contacts");
+                _categories.AddLast("OneDrive");
+                _categories.AddLast("Excel");
+                _categories.AddLast("Planner");
+                _categories.AddLast("Insights");
+                _categories.AddLast("Insights (beta)");
+                _categories.AddLast("People");
+                _categories.AddLast("Extensions");
+                _categories.AddLast("OneNote");
+                _categories.AddLast("SharePoint Sites");
+                _categories.AddLast("SharePoint Lists");
+                _categories.AddLast("Batching");
+                _categories.AddLast("Microsoft Teams");
+                _categories.AddLast("Microsoft Teams (beta)");
+                _categories.AddLast("Security");
+                _categories.AddLast("User Activities");
+                _categories.AddLast("Applications (beta)");
+                _categories.AddLast("Notifications (beta)");
+            }
+        }
+
+        private StringBuilder GetListOfCategories()
+        {
+            StringBuilder categoriesStringCollection = new StringBuilder();
+          
+            foreach (string category in Categories)
+            {
+                categoriesStringCollection.Append(category + "\r\n");
+            }
+
+            return categoriesStringCollection;
+        }
+       
 
         public class Header
         {
