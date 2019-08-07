cd7253e3073a342ce7580fac158de4fe82cfafc2
Irvine Sunday <v-irsund@microsoft.com>
2019-08-07 18:38:08 +0300

    Deleting unnecessary classes
    These were pulled in from the dev merge into the query-string-params branch

--- GraphExplorerSamplesService/SampleQueriesList.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace GraphExplorerSamplesService
-{
-    public class SampleQueriesList
-    {
-        public List<SampleQueryModel> SampleQueries { get; set; }
-    }
-}
