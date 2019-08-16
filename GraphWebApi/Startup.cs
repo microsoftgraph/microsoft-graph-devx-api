269fe6752b25d62cbd4b0426f83a0e2c52dda344
Irvine Sunday <v-irsund@microsoft.com>
2019-08-17 00:50:15 +0300

    Updating authorization and removing unnecessary usings

--- GraphWebApi/Startup.cs
+++ GraphWebApi/Startup.cs
@@ -7,8 +7,8 @@ using Microsoft.Extensions.DependencyInjection;
 using CodeSnippetsReflection;
 using GraphExplorerSamplesService.Services;
 using GraphExplorerSamplesService.Interfaces;
-using Microsoft.AspNetCore.Authentication.AzureAD.UI;
-using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 namespace GraphWebApi
 {
@@ -24,12 +24,20 @@ namespace GraphWebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddAuthentication(AzureADDefaults.BearerAuthenticationScheme)
-                .AddAzureADBearer(options =>
-                {
-                    Configuration.Bind("AzureAd", options);
-                    
-                });
+            services.AddAuthentication(option =>
+            {
+                option.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+                   .AddJwtBearer(option =>
+                   {
+                       option.Authority = string.Format("{0}{1}", Configuration["AzureAd:Instance"], Configuration["AzureAd:TenantId"]);
+                       option.TokenValidationParameters = new TokenValidationParameters
+                       {
+                           ValidAudience = Configuration["AzureAd:Audience"],
+                           ValidIssuer = Configuration["AzureAd:Issuer"]
+                       };
+                   });
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             services.AddSingleton<ISnippetsGenerator, SnippetsGenerator>();
             services.AddSingleton<IFileUtility, DiskFileUtility>();
