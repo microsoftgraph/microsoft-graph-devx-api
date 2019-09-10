5699ddee8c699ba5cf801fc48fe37eeb6727feb6
Irvine Sunday <v-irsund@microsoft.com>
2019-09-10 14:59:51 +0300

    Adding config. options class for samples admins.

---
+++ GraphWebApi/Models/SamplesAdministrators.cs
@@ -0,0 +1,10 @@
+﻿namespace GraphWebApi.Models
+{
+    /// <summary>
+    /// Defines a list of the samples administrators 
+    /// </summary>
+    public class SamplesAdministrators
+    {
+        public string[] Administrators { get; set; }
+    }
+}
