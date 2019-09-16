ce9bb8ecb4fd228fb0d3b342c1c5d212e59dfbd4
Irvine Sunday <v-irsund@microsoft.com>
2019-09-16 12:11:35 +0300

    Adding the permissions service lib. and other library dependencies

---
+++ GraphExplorerPermissionsService/Interfaces/IPermissionsStore.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GraphExplorerPermissionsService.Interfaces
+{
+    public interface IPermissionsStore
+    {
+        string[] GetScopes(string requestUrl, string method = "GET", string scopeType = "Application");
+    }
+}
