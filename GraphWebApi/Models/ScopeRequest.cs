272305f24cccb375e9b74095ad63cad513bbebb8
Irvine Sunday <v-irsund@microsoft.com>
2019-09-17 13:14:24 +0300

    Adding model to represent the permission scope request

---
+++ GraphWebApi/Models/ScopeRequest.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+
+namespace GraphWebApi.Models
+{
+    /// <summary>
+    /// Defines the model for a permission scope request.
+    /// </summary>
+    public class ScopeRequest
+    {
+        [Required(ErrorMessage = "The RequestUrl parameter is required in the query string.")]
+        public string RequestUrl { get; set; }
+        public string HttpVerb { get; set; } = "GET";
+        public string ScopeType { get; set; } = "Application";     
+    }
+}
