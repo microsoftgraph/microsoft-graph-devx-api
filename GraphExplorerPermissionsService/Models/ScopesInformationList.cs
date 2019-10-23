1c99a5fc07e7116f727cf656c846ee42defaa369
Irvine Sunday <v-irsund@microsoft.com>
2019-10-23 21:54:54 +0300

    Adding models that define the scopes information

---
+++ GraphExplorerPermissionsService/Models/ScopesInformationList.cs
@@ -0,0 +1,23 @@
+﻿// ------------------------------------------------------------------------------------------------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
+// ------------------------------------------------------------------------------------------------------------------------------------------------------
+
+using System.Collections.Generic;
+
+namespace GraphExplorerPermissionsService.Models
+{
+    /// <summary>
+    /// Defines a list which holds a collection of delegated and application <see cref="ScopeInformation"/> objects.
+    /// </summary>
+    public class ScopesInformationList
+    {
+        public List<ScopeInformation> DelegatedScopesList { get; set; }
+        public List<ScopeInformation> ApplicationScopesList { get; set; }
+
+        public ScopesInformationList()
+        {
+            DelegatedScopesList = new List<ScopeInformation>();
+            ApplicationScopesList = new List<ScopeInformation>();
+        }
+    }
+}
