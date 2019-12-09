27e1f0d29729b98c977bb7bf3e550586a018f203
Irvine Sunday <v-irsund@microsoft.com>
2019-12-09 15:07:41 +0300

    Adding OpenApi service controllers

---
+++ GraphWebApi/Controllers/IndexController.cs
@@ -0,0 +1,57 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using GraphWebApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Graph.OpenAPIService;
+using Microsoft.OpenApi.Models;
+using Microsoft.OpenApi.Services;
+
+namespace GraphWebApi.Controllers
+{
+    [Route("list")]
+    [ApiController]
+    public class IndexController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get(string graphVersion = "v1.0", bool forceRefresh = false)
+        {
+            var graphOpenApi = await OpenApiService.GetGraphOpenApiDocument(graphVersion,forceRefresh);
+            WriteIndex(this.Request.Scheme + "://"+ this.Request.Host.Value, graphVersion, graphOpenApi, Response.Body);
+            
+            return new EmptyResult();
+        }
+
+        private static void WriteIndex(string baseUrl, string graphVersion, OpenApiDocument graphOpenApi, Stream stream)
+        {
+            var sw = new StreamWriter(stream);
+            
+            var indexSearch = new OpenApiOperationIndex();
+            var walker = new OpenApiWalker(indexSearch);
+
+            walker.Walk(graphOpenApi);
+
+            sw.AutoFlush = true;
+
+            sw.WriteLine("<head>");
+            sw.WriteLine("<link rel='stylesheet' href='./stylesheet.css' />");
+            sw.WriteLine("</head>");
+            sw.WriteLine("<h1># OpenAPI Operations for Microsoft Graph</h1>");
+            sw.WriteLine("<b/>");
+            sw.WriteLine("<ul>");
+            foreach (var item in indexSearch.Index)
+            {
+                
+                var target = $"{baseUrl}/openapi?tags={item.Key.Name}&openApiVersion=3&graphVersion={graphVersion}";
+                sw.WriteLine($"<li>{item.Key.Name} [<a href='./openapi?tags={target}'>OpenApi</a>]   [<a href='/swagger/index.html#url={target}'>Swagger UI</a>]</li>");
+                sw.WriteLine("<ul>");
+                foreach (var op in item.Value)
+                {
+                    sw.WriteLine($"<li>{op.OperationId}  [<a href='./openapi?operationIds={op.OperationId}'>OpenAPI</a>]</li>");
+                }
+                sw.WriteLine("</ul>");
+            }
+            sw.WriteLine("</ul>");
+
+        }
+    }
+}
---
+++ GraphWebApi/Controllers/OpenApiController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Graph.OpenAPIService;
+using Microsoft.OpenApi.Models;
+using System.Threading.Tasks;
+
+namespace GraphWebApi.Controllers
+{
+    /// <summary>
+    /// Controller that enables querying over an OpenAPI document
+    /// </summary>
+    public class OpenApiController : ControllerBase
+    {
+        [Route("openapi")]
+        [Route("$openapi")]
+        [HttpGet]
+        public async Task<IActionResult> Get(
+                                    [FromQuery]string operationIds = null,
+                                    [FromQuery]string tags = null,
+                                    [FromQuery]string openApiVersion = "2",
+                                    [FromQuery]string title = "Partial Graph API",
+                                    [FromQuery]OpenApiStyle style = OpenApiStyle.Plain,
+                                    [FromQuery]string format = "yaml",
+                                    [FromQuery]string graphVersion = "v1.0",
+                                    [FromQuery]bool forceRefresh = false)
+        {
+            var predicate = OpenApiService.CreatePredicate(operationIds, tags);
+
+            if (predicate == null)
+            {
+                return new BadRequestResult();
+            }
+
+            OpenApiDocument source = await OpenApiService.GetGraphOpenApiDocument(graphVersion, forceRefresh);
+
+            var subsetOpenApiDocument = OpenApiService.CreateFilteredDocument(source, title, graphVersion, predicate);
+
+            subsetOpenApiDocument = OpenApiService.ApplyStyle(style, subsetOpenApiDocument);
+
+            var stream = OpenApiService.SerializeOpenApiDocument(subsetOpenApiDocument, openApiVersion, format);
+            return new FileStreamResult(stream, "application/json");
+        }
+    }
+}
