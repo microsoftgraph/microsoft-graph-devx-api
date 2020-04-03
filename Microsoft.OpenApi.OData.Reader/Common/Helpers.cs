d124bd9b450b75f11f0605d5943dd137d41d7c8f
Irvine Sunday <40403681+irvinesunday@users.noreply.github.com>
2020-04-03 10:57:28 +0300

    Updates the OData.Reader project to get latest feature updates (#215)
    * Updates the OData.Reader lib.
    
    * Removes unnecessary library package
    
    * Bumps up version of lib to get bug fix
    
    * Adds changes from the OData.Reader library
    
    Co-authored-by: Irvine Sunday <v-irsund@microsoft.com>

---
+++ Microsoft.OpenApi.OData.Reader/Common/Helpers.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.OData.Edm;
+using Microsoft.OpenApi.Models;
+
+namespace Microsoft.OpenApi.OData.Common
+{
+    internal static class Helpers
+    {
+        /// <summary>
+        /// Adds the derived types references together with their base type reference in the OneOf property of an OpenAPI schema.
+        /// </summary>
+        /// <returns>The OpenAPI schema with the list of derived types references and their base type references set in the OneOf property.</returns>
+        internal static OpenApiSchema GetDerivedTypesReferenceSchema(IEdmEntityType entityType, IEdmModel edmModel)
+        {
+            IEnumerable<IEdmEntityType> derivedTypes = edmModel.FindDirectlyDerivedTypes(entityType).OfType<IEdmEntityType>();
+
+            if (!derivedTypes.Any())
+            {
+                return null;
+            }
+
+            OpenApiSchema schema = new OpenApiSchema
+            {
+                OneOf = new List<OpenApiSchema>()
+            };
+
+            OpenApiSchema baseTypeSchema = new OpenApiSchema
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.Schema,
+                    Id = entityType.FullName()
+                }
+            };
+            schema.OneOf.Add(baseTypeSchema);
+
+            foreach (IEdmEntityType derivedType in derivedTypes)
+            {
+                OpenApiSchema derivedTypeSchema = new OpenApiSchema
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.Schema,
+                        Id = derivedType.FullName()
+                    }
+                };
+                schema.OneOf.Add(derivedTypeSchema);
+            };
+
+            return schema;
+        }
+    }
+}
