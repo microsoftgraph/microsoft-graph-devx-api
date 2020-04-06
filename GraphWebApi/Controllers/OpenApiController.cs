9a501177be3d14a3c385d5fbd8e04d12a62fabf9
Irvine Sunday <v-irsund@microsoft.com>
2020-04-06 11:05:24 +0300

    Adds copyright headers

--- GraphWebApi/Controllers/OpenApiController.cs
+++ GraphWebApi/Controllers/OpenApiController.cs
@@ -1,3 +1,7 @@
+// ------------------------------------------------------------------------------------------------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
+// ------------------------------------------------------------------------------------------------------------------------------------------------------
+
 using GraphWebApi.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
