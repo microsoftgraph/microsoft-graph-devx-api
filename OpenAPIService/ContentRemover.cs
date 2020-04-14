aa214c66577de0999f8c4d0f60f811662bf2654e
Irvine Sunday <v-irsund@microsoft.com>
2020-04-14 10:57:30 +0300

    Visitor class that removes the Content properties for response/requestBody

---
+++ OpenAPIService/ContentRemover.cs
@@ -0,0 +1,31 @@
+﻿// ------------------------------------------------------------------------------------------------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
+// ------------------------------------------------------------------------------------------------------------------------------------------------------
+
+using Microsoft.OpenApi.Models;
+using Microsoft.OpenApi.Services;
+using System.Linq;
+
+namespace OpenAPIService
+{
+    internal class ContentRemover: OpenApiVisitorBase
+    {       
+        public override void Visit(OpenApiResponse response)
+        {            
+            if (response.Content.Any() == true)
+            {
+                response.Content.Clear();
+                base.Visit(response);
+            }
+        }
+
+        public override void Visit(OpenApiRequestBody requestBody)
+        {
+            if (requestBody.Content.Any() == true)
+            {
+               requestBody.Content.Clear();
+               base.Visit(requestBody);
+            }         
+        }
+    }
+}
