354dc0aadd2669fe540a6656c4f91a8fb62db091
Irvine Sunday <v-irsund@microsoft.com>
2020-04-20 15:22:42 +0300

    Update default property values to be more descriptive

--- GraphExplorerPermissionsService/Models/ScopeInformation.cs
+++ GraphExplorerPermissionsService/Models/ScopeInformation.cs
@@ -12,11 +12,11 @@ namespace GraphExplorerPermissionsService.Models
     public class ScopeInformation
     {
         [JsonProperty(Required = Required.Always, PropertyName = "value")]
-        public string ScopeName { get; set; } = "Undefined";
+        public string ScopeName { get; set; } = "Scope name unavailable";
         [JsonProperty(Required = Required.Always, PropertyName = "consentDisplayName")]
-        public string DisplayName { get; set; } = "Undefined";
+        public string DisplayName { get; set; } = "Consent name unavailable";
         [JsonProperty(Required = Required.Always, PropertyName = "consentDescription")]
-        public string Description { get; set; } = "Undefined";
+        public string Description { get; set; } = "Consent description unavailable";
         [JsonProperty(Required = Required.Always, PropertyName = "isAdmin")]
         public bool IsAdmin { get; set; }
     }
