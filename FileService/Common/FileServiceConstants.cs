4470af8d024a41871553e6b15d930fd81aafbd6f
Irvine Sunday <v-irsund@microsoft.com>
2020-04-20 09:02:56 +0300

    Create constant file that defines path separator

---
+++ FileService/Common/FileServiceConstants.cs
@@ -0,0 +1,14 @@
+﻿// ------------------------------------------------------------------------------------------------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
+// ------------------------------------------------------------------------------------------------------------------------------------------------------
+
+namespace FileService.Common
+{
+    /// <summary>
+    /// Defines constants for the File Service.
+    /// </summary>
+    public static class FileServiceConstants
+    {
+        public const char DirectorySeparator = '\\';
+    }
+}
