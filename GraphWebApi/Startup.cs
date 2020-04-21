88937c41c9472f1fd457f0523f5f8a0497a9a816
Irvine Sunday <v-irsund@microsoft.com>
2020-04-21 08:53:35 +0300

    Add license headers

--- GraphWebApi/Startup.cs
+++ GraphWebApi/Startup.cs
@@ -1,3 +1,6 @@
+// ------------------------------------------------------------------------------------------------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
+// ------------------------------------------------------------------------------------------------------------------------------------------------------
 
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
