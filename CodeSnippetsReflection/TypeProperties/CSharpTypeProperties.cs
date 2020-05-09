c754fb1b26ebe975333aec41d08f5af1c04d7372
Mustafa Zengin <muzengin@microsoft.com>
2020-05-08 20:02:20 -0700

    add support for OpenTypes, which can have arbitrary properties. These properties should appear in AdditionalData structure in SDKs.

---
+++ CodeSnippetsReflection/TypeProperties/CSharpTypeProperties.cs
@@ -0,0 +1,41 @@
+﻿using CodeSnippetsReflection.LanguageGenerators;
+using Microsoft.OData.Edm;
+using System.Linq;
+
+namespace CodeSnippetsReflection.TypeProperties
+{
+    public class CSharpTypeProperties
+    {
+        /// <summary>
+        /// returns C# class name from graph type: microsoft.graph.data => Data
+        /// </summary>
+        public string ClassName => CommonGenerator.UppercaseFirstLetter(EdmType.ToString().Split(".").Last());
+
+        /// <summary>
+        /// Whether the type is open, i.e. supports arbitrary properties (which goes in AdditionalData dictionary in SDK)
+        /// </summary>
+        public bool IsOpenType => EdmType.IsOpen();
+
+        /// <summary>
+        /// Edm type
+        /// </summary>
+        public readonly IEdmType EdmType;
+
+        /// <summary>
+        /// Whether found property is a navigation property or not
+        /// Used in decision for ICollectionPage casting
+        /// </summary>
+        public readonly bool IsNavigationProperty;
+
+        /// <summary>
+        /// initializes a properties object
+        /// </summary>
+        /// <param name="edmType">edm type</param>
+        /// <param name="isNavigationProperty">Whether found property is a navigation property or not</param>
+        public CSharpTypeProperties(IEdmType edmType, bool isNavigationProperty)
+        {
+            EdmType = edmType;
+            IsNavigationProperty = isNavigationProperty;
+        }
+    }
+}
