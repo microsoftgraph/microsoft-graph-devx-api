8493b5ff1c803c7c9c6a58faad2ed5646fbb37db
Irvine Sunday <irochand@microsoft.com>
2020-07-07 15:12:52 +0300

    Add License information

--- OpenAPIService/PowershellFormatter.cs
+++ OpenAPIService/PowershellFormatter.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.OpenApi.Models;
+﻿// ------------------------------------------------------------------------------------------------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
+// ------------------------------------------------------------------------------------------------------------------------------------------------------
+
+using Microsoft.OpenApi.Models;
 using Microsoft.OpenApi.Services;
 using System.Text;
 
@@ -16,7 +20,7 @@ namespace OpenAPIService
         /// </summary>
         public override void Visit(OpenApiOperation operation)
         {
-            var operationId = operation.OperationId;                        
+            var operationId = operation.OperationId;
 
             int charPos = operationId.LastIndexOf('.', operationId.Length - 1);
             if (charPos >= 0)
