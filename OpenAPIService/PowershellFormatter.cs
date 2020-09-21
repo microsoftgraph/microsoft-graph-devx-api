1489aa2e5c44b52b024e4bac42fc4f71cc952420
Irvine Sunday <irochand@microsoft.com>
2020-09-21 23:37:03 +0300

    Update operation ids for Put operations only

--- OpenAPIService/PowershellFormatter.cs
+++ OpenAPIService/PowershellFormatter.cs
@@ -13,8 +13,31 @@ namespace OpenAPIService
    /// </summary>
     internal class PowershellFormatter : OpenApiVisitorBase
     {
-        private const string DefaultPatchPrefix = ".Update";
-        private const string NewPatchPrefix = "_Set";
+        private const string DefaultPutPrefix = ".Update";
+        private const string NewPutPrefix = "_Set";
+
+        /// <summary>
+        /// Accesses the individual OpenAPI operations for a particular OpenApiPathItem.
+        /// </summary>
+        /// <param name="pathItem">The OpenApiPathItem.</param>
+        public override void Visit(OpenApiPathItem pathItem)
+        {
+            var putOperation = OperationType.Put;
+
+            // For PowerShell, Put operation id should have the format -> {xxx}_Set{Yyy}
+            if (pathItem.Operations.ContainsKey(putOperation))
+            {
+                var operationId = pathItem.Operations[putOperation].OperationId;
+
+                if (operationId.Contains(DefaultPutPrefix))
+                {
+                    StringBuilder newOperationId = new StringBuilder(operationId);
+
+                    newOperationId.Replace(DefaultPutPrefix, NewPutPrefix);
+                    pathItem.Operations[putOperation].OperationId = newOperationId.ToString();
+                }
+            }
+        }
 
         /// <summary>
         /// The last '.' character of the OperationId value separates the method group from the operation name.
@@ -25,27 +48,21 @@ namespace OpenAPIService
             var operationId = operation.OperationId;
 
             int charPos = operationId.LastIndexOf('.', operationId.Length - 1);
-            if (charPos >= 0)
+
+            // Check whether Put operation id already got updated
+            if (charPos >= 0 && !operationId.Contains(NewPutPrefix))
             {
                 StringBuilder newOperationId = new StringBuilder(operationId);
 
-                // Patch operation id should have the format -> {xxx}_Set{Yyy}
-                if (operationId.Contains(DefaultPatchPrefix))
-                {
-                    newOperationId.Replace(DefaultPatchPrefix, NewPatchPrefix);
-                }
-                else
-                {
-                    newOperationId[charPos] = '_';
-                }
-
+                newOperationId[charPos] = '_';
                 operation.OperationId = newOperationId.ToString();
             }
         }
 
         public override void Visit(OpenApiSchema schema)
         {
-            if (schema?.Type == "object") {
+            if (schema?.Type == "object")
+            {
                 schema.AdditionalProperties = new OpenApiSchema() {Type = "object"};  // To make AutoREST happy
             }
             base.Visit(schema);
