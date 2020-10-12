034a6f7cd27d56de03f2c109b1b216fa21f14451
Irvine Sunday <irochand@microsoft.com>
2020-10-12 13:18:46 +0300

    Update tests

--- UriMatchService.Test/UriTemplateMatcherShould.cs
+++ UriMatchService.Test/UriTemplateMatcherShould.cs
@@ -13,11 +13,14 @@ namespace UriMatchingService.Test
         InlineData("/", "root"),
         InlineData("/baz/fod/burg", ""),
         InlineData("/baz/kit", "kit"),
+        InlineData("/games/monopoly/Setup/23", "gamessetup"),
         InlineData("http://www.example.com/baz/fod", "baz"),
         InlineData("/baz/fod/blob", "blob"),
         InlineData("/glah/flid/blob", "goo"),
-        InlineData("/settings/{12345}", "set"),
-        InlineData("/organization/{54321}/settings/iteminsights", "org")]
+        InlineData("/settings/12345", "set"),
+        InlineData("/organization/54321/settings/iteminsights", "org"),
+        InlineData("/games/monopoly/22/State/33", "state"),
+        InlineData("/foo?x=1&y=2", "fooxy3")]
         public void MatchPathToUriTemplates(string uri, string key)
         {
             // Arrange
@@ -25,11 +28,14 @@ namespace UriMatchingService.Test
             table.Add("root", "/");
             table.Add("foo", "/foo/{bar}");
             table.Add("kit", "/baz/kit");
+            table.Add("fooxy3", "/foo?x={x}&y={y}");
             table.Add("baz", "/baz/{bar}");
             table.Add("blob", "/baz/{bar}/blob");
             table.Add("goo", "/{goo}/{bar}/blob");
             table.Add("set", "/settings/{id}");
+            table.Add("state", "/games/{gametitle}/{gameid}/State/{stateid}");
             table.Add("org", "/organization/{id}/settings/iteminsights");
+            table.Add("gamessetup", "/games/{gametitle}/Setup/{gamesid}");
 
             // Act
             var result = table.Match(new Uri(uri, UriKind.RelativeOrAbsolute));
