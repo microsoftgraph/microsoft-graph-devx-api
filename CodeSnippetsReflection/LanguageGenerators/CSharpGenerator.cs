1b57d9a364cd778d2367278794162e8d7e5ce4b9
Mustafa Zengin <muzengin@microsoft.com>
2020-10-29 23:15:22 -0700

    Fix reference assignment (#348)
    * fix id assignment
    
    * fix reference segment
    
    * fix id parsing logic

--- CodeSnippetsReflection/LanguageGenerators/CSharpGenerator.cs
+++ CodeSnippetsReflection/LanguageGenerators/CSharpGenerator.cs
@@ -121,10 +121,25 @@ namespace CodeSnippetsReflection.LanguageGenerators
                         throw new Exception($"No request Body present for PUT of entity {snippetModel.ResponseVariableName}");
 
                     var genericType = string.Empty;
+                    var objectToBePut = snippetModel.ResponseVariableName;
                     if (snippetModel.ContentType.Equals("application/json", StringComparison.OrdinalIgnoreCase))
                     {
-                        snippetBuilder.Append($"var {snippetModel.ResponseVariableName} = ");
-                        snippetBuilder.Append(CSharpGenerateObjectFromJson(segment, snippetModel.RequestBody, new List<string> { snippetModel.ResponseVariableName }));
+                        if (snippetModel.Segments.Last() is NavigationPropertyLinkSegment)
+                        {
+                            // if we are putting reference, we should send id to that object in PutAsync()
+                            // and the request body should contain a JSON with @odata.id key
+                            var body = JsonConvert.DeserializeObject(snippetModel.RequestBody) as JObject;
+
+                            // HTTP sample is in this format: https://graph.microsoft.com/v1.0/users/{id}
+                            // but C# SDK reconstructs the URL from {id}
+                            var id = body["@odata.id"].ToString().Split("/").Last();
+                            objectToBePut = $"\"{id}\"";
+                        }
+                        else
+                        {
+                            snippetBuilder.Append($"var {snippetModel.ResponseVariableName} = ");
+                            snippetBuilder.Append(CSharpGenerateObjectFromJson(segment, snippetModel.RequestBody, new List<string> { snippetModel.ResponseVariableName }));
+                        }
                     }
                     else
                     {
@@ -138,7 +153,7 @@ namespace CodeSnippetsReflection.LanguageGenerators
                         }
                     }
 
-                    snippetBuilder.Append(GenerateRequestSection(snippetModel, $"{actions}\n\t.PutAsync{genericType}({snippetModel.ResponseVariableName});"));
+                    snippetBuilder.Append(GenerateRequestSection(snippetModel, $"{actions}\n\t.PutAsync{genericType}({objectToBePut});"));
 
                 }
                 else
