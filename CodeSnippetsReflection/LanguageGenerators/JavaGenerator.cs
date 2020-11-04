a89faed7b08d37ad6d5257607b42e03ca19f2d27
Vincent Biret <vibiret@microsoft.com>
2020-11-04 10:31:48 -0500

    - fixes a bug where java generation would try to parse binary payloads and fail

--- CodeSnippetsReflection/LanguageGenerators/JavaGenerator.cs
+++ CodeSnippetsReflection/LanguageGenerators/JavaGenerator.cs
@@ -106,7 +106,10 @@ namespace CodeSnippetsReflection.LanguageGenerators
                     if (string.IsNullOrEmpty(snippetModel.RequestBody))
                         throw new Exception("No request Body present for Java Put request");
 
-                    snippetBuilder.Append(JavaGenerateObjectFromJson(segment, snippetModel.RequestBody, new List<string> { snippetModel.ResponseVariableName }));
+                    if (snippetModel.ContentType.Contains("json"))
+                        snippetBuilder.Append(JavaGenerateObjectFromJson(segment, snippetModel.RequestBody, new List<string> { snippetModel.ResponseVariableName }));
+                    else
+                        snippetBuilder.Append($"InputStream {snippetModel.ResponseVariableName} = new ByteArrayInputStream(Base64.getDecoder().decode({AddQuotesIfMising(snippetModel.RequestBody)}));\n\t");
                     snippetBuilder.Append(GenerateRequestSection(snippetModel, $"{requestActions}\n\t.put({snippetModel.ResponseVariableName});"));
                 }
                 else if (snippetModel.Method == HttpMethod.Delete)
