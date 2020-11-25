c97d1df14925dee1edfcb365f6381dc7db539adf
Maggie Kimani <maggiekim42@gmail.com>
2020-11-25 16:00:44 +0300

    Adds an IPermissionsTestStore that defines a method for fetching permissions files for testing from github

---
+++ GraphExplorerPermissionsService/Interfaces/IPermissionsTestStore.cs
@@ -0,0 +1,11 @@
+﻿using GraphExplorerPermissionsService.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace GraphExplorerPermissionsService.Interfaces
+{
+    public interface IPermissionsTestStore
+    {
+        Task<List<ScopeInformation>> FetchPermissionsDescriptionsFromGithub(string locale, string org, string branchName);
+    }
+}
