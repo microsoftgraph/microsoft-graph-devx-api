966c88eb5ec93df01e5594d5abb3f2f8467370e8
Maggie Kimani <maggiekim42@gmail.com>
2020-11-25 15:58:51 +0300

    Adds an ISampleTestStore interface that defines a method for fetching sample queries for testing from github

---
+++ GraphExplorerSamplesService/Interfaces/ISamplesTestStore.cs
@@ -0,0 +1,14 @@
+﻿// ------------------------------------------------------------------------------------------------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
+// ------------------------------------------------------------------------------------------------------------------------------------------------------
+
+using GraphExplorerSamplesService.Models;
+using System.Threading.Tasks;
+
+namespace GraphExplorerSamplesService.Interfaces
+{
+    public interface ISamplesTestStore
+    {
+        Task<SampleQueriesList> FetchSampleQueriesFromGithub(string locale, string org, string branchName);
+    }
+}
