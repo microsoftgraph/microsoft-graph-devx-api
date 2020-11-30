ead23687c70dba1f421a45178714fe9082848dcf
Maggie Kimani <maggiekim42@gmail.com>
2020-11-30 17:23:45 +0300

    Add sample queries file url to configuration and include localized file versions

--- GraphExplorerSamplesService/Services/SamplesTestStore.cs
+++ GraphExplorerSamplesService/Services/SamplesTestStore.cs
@@ -2,8 +2,11 @@
 //  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
 // ------------------------------------------------------------------------------------------------------------------------------------------------------
 
+using FileService.Common;
+using FileService.Interfaces;
 using GraphExplorerSamplesService.Interfaces;
 using GraphExplorerSamplesService.Models;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -15,12 +18,21 @@ namespace GraphExplorerSamplesService.Services
     /// </summary>
     public class SamplesTestStore : ISamplesTestStore
     {
-        //private readonly HttpClient _httpClient;
         private readonly IHttpClientFactory _httpClientFactory;
+        private readonly IConfiguration _configuration;
+        private readonly string _sampleQueriesContainerName;
+        private readonly string _sampleQueriesBlobName;
+        private readonly string  _host;
+        private readonly string _repo;
 
-        public SamplesTestStore(IHttpClientFactory httpClientFactory)
+        public SamplesTestStore(IHttpClientFactory httpClientFactory, IFileUtility fileUtility, IConfiguration configuration)
         {
             _httpClientFactory = httpClientFactory;
+            _configuration = configuration;
+            _host = _configuration["BlobStorage:GithubHost"];
+            _repo = _configuration["BlobStorage:RepoName"];
+            _sampleQueriesContainerName = _configuration["BlobStorage:Containers:SampleQueries"];
+            _sampleQueriesBlobName = _configuration[$"BlobStorage:Blobs:SampleQueries"];
         }
 
         /// <summary>
@@ -31,25 +43,30 @@ namespace GraphExplorerSamplesService.Services
         /// <param name="org"></param>
         /// <param name="branchName"></param>
         /// <returns>The deserialized instance of a <see cref="SampleQueriesList"/>.</returns>
-        public async Task<SampleQueriesList> FetchSampleQueriesFromGithub(string locale, string org, string branchName)
+        public async Task<SampleQueriesList> FetchSampleQueriesListAsync(string locale, string org, string branchName)
         {
-            var client = _httpClientFactory.CreateClient("github");
+            string source = "github";
+            var client = _httpClientFactory.CreateClient(source);
+            
+            /* download sample query file contents*/
 
-            // download sample query file contents
-            var samplesFilePath = string.Concat("https://raw.githubusercontent.com/", org, "/microsoft-graph-devx-content/", branchName, "/sample-queries/sample-queries.json");
-            var responseMessage = await client.GetAsync(samplesFilePath);
+            // Fetch the requisite sample path source based on the locale
+            string queriesFilePathSource = FileServiceHelper.GetLocalizedFilePathSource(_sampleQueriesContainerName, _sampleQueriesBlobName, locale, source);
 
-            //order English translation of the sample queries
-            bool orderSamples = locale.Equals("en-us", StringComparison.OrdinalIgnoreCase);
+            var completeFilePath = string.Concat(_host, org, _repo, branchName, FileServiceConstants.GithubDirectorySeparator, queriesFilePathSource);
+            var responseMessage = await client.GetAsync(completeFilePath);
 
             if (!(responseMessage.IsSuccessStatusCode))
             {
                 return null;
             }
-            
-            var fileContents = await responseMessage.Content.ReadAsStringAsync();
-            SampleQueriesList sampleQueriesList = SamplesService.DeserializeSampleQueriesList(fileContents, orderSamples);
 
+            var fileContents = await responseMessage.Content.ReadAsStringAsync();
+
+            //order English translation of the sample queries
+            bool orderSamples = locale.Equals("en-us", StringComparison.OrdinalIgnoreCase);
+            
+            SampleQueriesList sampleQueriesList = SamplesService.DeserializeSampleQueriesList(fileContents, orderSamples);
             
             return sampleQueriesList;
         }
