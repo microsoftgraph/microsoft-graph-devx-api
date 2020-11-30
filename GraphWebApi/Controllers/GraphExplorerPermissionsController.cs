64c14cdb360df2e8a6dfa36e3793ce066863cc19
Maggie Kimani <maggiekim42@gmail.com>
2020-11-30 19:48:52 +0300

    Restore deleted file

---
+++ GraphWebApi/Controllers/GraphExplorerPermissionsController.cs
@@ -0,0 +1,72 @@
+// ------------------------------------------------------------------------------------------------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
+// ------------------------------------------------------------------------------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using GraphExplorerPermissionsService.Interfaces;
+using GraphExplorerPermissionsService.Models;
+using GraphWebApi.Common;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GraphWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [Route("permissions")]
+    [ApiController]
+    public class GraphExplorerPermissionsController : ControllerBase
+    {
+        private readonly IPermissionsStore _permissionsStore;
+        private readonly IPermissionsTestStore _permissionsTestStore;
+
+        public GraphExplorerPermissionsController(IPermissionsStore permissionsStore, IPermissionsTestStore permissionsTestStore)
+        {
+            _permissionsStore = permissionsStore;
+            _permissionsTestStore = permissionsTestStore;
+        }
+
+        // Gets the permissions scopes
+        [HttpGet]
+        [Produces("application/json")]
+        public async Task<IActionResult> GetPermissionScopes([FromQuery]string scopeType = "DelegatedWork",
+                                                             [FromQuery]string requestUrl = null,
+                                                             [FromQuery]string method = null,
+                                                             [FromQuery] string org = null,
+                                                             [FromQuery] string branchName = null)
+        {
+            try
+            {
+                string localeCode = RequestHelper.GetPreferredLocaleLanguage(Request);
+
+                List<ScopeInformation> result = null;
+                if (!string.IsNullOrEmpty(org) && !string.IsNullOrEmpty(branchName))
+                {
+                    //Fetch permissions descriptions file from Github
+                    result = await _permissionsTestStore.FetchPermissionsDescriptionsFromGithub(localeCode, org, branchName);
+                }
+
+                else
+                {
+                    // Fetch permissions files from Azure Blob
+                    result = await _permissionsStore.GetScopesAsync(scopeType, localeCode, requestUrl, method);
+                }
+
+                return result == null ? NotFound() : (IActionResult)Ok(result);
+            }
+            catch (InvalidOperationException invalidOpsException)
+            {
+                return new JsonResult(invalidOpsException.Message) { StatusCode = StatusCodes.Status500InternalServerError };
+            }
+            catch (ArgumentNullException argNullException)
+            {
+                return new JsonResult(argNullException.Message) { StatusCode = StatusCodes.Status400BadRequest };
+            }
+            catch (Exception exception)
+            {
+                return new JsonResult(exception.Message) { StatusCode = StatusCodes.Status500InternalServerError };
+            }
+        }
+    }
+}
