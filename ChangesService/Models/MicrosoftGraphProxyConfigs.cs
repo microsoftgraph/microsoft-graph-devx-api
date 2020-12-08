52eab4fda0a29ad49402d6dae515686efe8075cd
Irvine Sunday <irochand@microsoft.com>
2020-12-08 13:50:49 +0300

    Changes Service configs.

---
+++ ChangesService/Models/MicrosoftGraphProxyConfigs.cs
@@ -0,0 +1,17 @@
+﻿// ------------------------------------------------------------------------------------------------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
+// ------------------------------------------------------------------------------------------------------------------------------------------------------
+
+namespace ChangesService.Models
+{
+    /// <summary>
+    /// Configs for connecting to the Microsoft Graph Proxy to
+    /// fetch workload names from Graph urls.
+    /// </summary>
+    public class MicrosoftGraphProxyConfigs
+    {
+        public string GraphVersion { get; set; } = "v1.0";
+        public string GraphProxyBaseUrl { get; set; } = "https://proxy.apisandbox.msdn.microsoft.com/";
+        public string GraphProxyRelativeUrl { get; set; } = "svc?url=https://graph.microsoft.com/{0}{1}?$whatif";
+    }
+}
