7805755b5b3c0e34d52959b51cd6b8255c447b09
Irvine Sunday <irochand@microsoft.com>
2020-12-08 13:45:40 +0300

    Changes Service constants

---
+++ ChangesService/Common/ChangesServiceConstants.cs
@@ -0,0 +1,25 @@
+﻿// ------------------------------------------------------------------------------------------------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
+// ------------------------------------------------------------------------------------------------------------------------------------------------------
+
+namespace ChangesService.Common
+{
+    /// <summary>
+    /// Constants for the ChangesService library.
+    /// </summary>
+    public static class ChangesServiceConstants
+    {
+        // Changelog configuration paths constants
+        public static string ChangelogRelativeUrlConfigPath = "BlobStorage:Blobs:ChangeLog:RelativeUrl";
+        public static string ChangelogBaseUrlConfigPath = "BlobStorage:Blobs:ChangeLog:BaseUrl";
+        public static string ChangelogRefreshTimeConfigPath = "FileCacheRefreshTimeInHours:ChangeLog";
+
+        // Microsoft Graph proxy configuration paths constants
+        public static string GraphProxyBaseUrlConfigPath = "GraphProxy:BaseUrl";
+        public static string GraphProxyRelativeUrlConfigPath = "GraphProxy:RelativeUrl";
+
+        // Error constants
+        public static string ValueNullError = "Value cannot be null.";
+        public static string JsonStringNullOrEmpty = "The JSON string to be deserialized cannot be null or empty.";
+    }
+}
