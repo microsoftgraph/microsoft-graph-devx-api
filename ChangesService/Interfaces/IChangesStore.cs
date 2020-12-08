c7ef1619ab73ca4f18cfc8f995f2f999aed23991
Irvine Sunday <irochand@microsoft.com>
2020-12-08 13:45:58 +0300

    Changes Service interfaces

---
+++ ChangesService/Interfaces/IChangesStore.cs
@@ -0,0 +1,17 @@
+﻿// ------------------------------------------------------------------------------------------------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
+// ------------------------------------------------------------------------------------------------------------------------------------------------------
+
+using ChangesService.Models;
+using System.Threading.Tasks;
+
+namespace ChangesService.Interfaces
+{
+    /// <summary>
+    /// Defines a contract for fetching changelog data.
+    /// </summary>
+    public interface IChangesStore
+    {
+        Task<ChangeLogList> FetchChangeLogListAsync(string locale);
+    }
+}
