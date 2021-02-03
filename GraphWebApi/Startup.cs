e494d41dc71d4c08882c02905044764a5cf9e464
Maggie Kimani <maggiekim42@gmail.com>
2021-02-03 09:55:11 +0300

    Remove CORS settings for local testing

--- GraphWebApi/Startup.cs
+++ GraphWebApi/Startup.cs
@@ -37,10 +37,6 @@ namespace GraphWebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddCors(options =>
-            {
-                options.AddDefaultPolicy(builder => builder.AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod());
-            });
             services.AddAuthentication(option =>
             {
                 option.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
@@ -106,7 +102,6 @@ namespace GraphWebApi
             app.UseHttpsRedirection();
             app.UseAuthentication();
             app.UseRouting();
-            app.UseCors();
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
