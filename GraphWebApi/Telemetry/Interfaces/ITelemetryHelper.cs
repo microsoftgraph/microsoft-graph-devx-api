e4d19801af5590d3678e5f8555709d80305d223e
Maggie Kimani <maggiekim42@gmail.com>
2021-03-09 10:20:52 +0300

    Add an ITelemetryHelper interface

---
+++ GraphWebApi/Telemetry/Interfaces/ITelemetryHelper.cs
@@ -0,0 +1,15 @@
+﻿// ------------------------------------------------------------------------------------------------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
+// ------------------------------------------------------------------------------------------------------------------------------------------------------
+
+using Microsoft.AspNetCore.Http;
+using System.Diagnostics;
+
+namespace GraphWebApi.Telemetry.Interfaces
+{
+    public interface ITelemetryHelper
+    {
+        public Stopwatch BeginRequest();
+        public void EndRequest(Stopwatch stopwatch, HttpContext httpContext, string eventName);
+    }
+}
