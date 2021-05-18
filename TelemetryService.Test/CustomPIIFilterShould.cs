68a05f9525f5791010842d143951500fb9340848
Irvine Sunday <irochand@microsoft.com>
2021-05-18 15:07:35 +0300

    Update tests where appropriate

--- TelemetryService.Test/CustomPIIFilterShould.cs
+++ TelemetryService.Test/CustomPIIFilterShould.cs
@@ -25,14 +25,17 @@ namespace Telemetry.Test
             Assert.Throws<ArgumentNullException>(() => new CustomPIIFilter(null));
         }
 
-        [Fact]
-        public void SanitizeGUIDFromEventTelemetry()
+        [Theory]
+        [InlineData("/permissions?requestUrl=/users/20463493-79c2-4116-b87b-a20d06242e6a&method=GET",
+                    "/permissions?requestUrl=/users/****&method=GET")]
+        [InlineData("/permissions?requestUrl=/me/people/9f376303-1936-44a9-b4fd-7271483525bb/drives&method=GET",
+                    "/permissions?requestUrl=/me/people/****/drives&method=GET")]
+        public void SanitizeGUIDFromEventTelemetry(string requestPath, string expectedPath)
         {
             // Arrange
             var httpMethod = "GET";
             var statusCode = "200";
             var elapsed = "5000";
-            var requestPath = "/permissions?requestUrl=/users/1d201493-c13f-4e36-bd06-a20d06242e6a&method=GET";
             var renderedMessage = $"HTTP {httpMethod + requestPath} responded {statusCode} in {elapsed} ms";
 
             var eventTelemetry = new EventTelemetry();
@@ -47,7 +50,7 @@ namespace Telemetry.Test
                 _telemetryProcessor.Process(eventTelemetry);
             }
 
-            var expectedPath = "/permissions?requestUrl=/users/****&method=GET";
+            //var expectedPath = "/permissions?requestUrl=/users/****&method=GET";
             var expectedMessage = $"HTTP {httpMethod + expectedPath} responded {statusCode} in {elapsed} ms";
 
             // Assert
@@ -63,7 +66,7 @@ namespace Telemetry.Test
             var statusCode = "200";
             var elapsed = "5000";
             var requestPath = "openapi?url=/users?$filter(emailAddress eq 'MiriamG@M365x214355.onmicrosoft.com')";
-            var renderedMessage = $"HTTP {httpMethod + requestPath} responded {statusCode} in {elapsed} ms";
+            var renderedMessage = $"HTTP {httpMethod} {requestPath} responded {statusCode} in {elapsed} ms";
 
             var eventTelemetry = new EventTelemetry();
             eventTelemetry.Properties.Add("RequestPath", requestPath);
@@ -78,7 +81,7 @@ namespace Telemetry.Test
             }
 
             var expectedPath = "openapi?url=/users?$filter(emailAddress eq '****')";
-            var expectedMessage = $"HTTP {httpMethod + expectedPath} responded {statusCode} in {elapsed} ms";
+            var expectedMessage = $"HTTP {httpMethod} {expectedPath} responded {statusCode} in {elapsed} ms";
 
             // Assert
             Assert.Equal(expectedPath, eventTelemetry.Properties["RequestPath"]);
@@ -93,7 +96,7 @@ namespace Telemetry.Test
             var statusCode = "200";
             var elapsed = "5000";
             var requestPath = "/openapi?url=/users?$filter(displayName eq 'Megan Bowen')";
-            var renderedMessage = $"HTTP {httpMethod + requestPath} responded {statusCode} in {elapsed} ms";
+            var renderedMessage = $"HTTP {httpMethod} {requestPath} responded {statusCode} in {elapsed} ms";
 
             var eventTelemetry = new EventTelemetry();
             eventTelemetry.Properties.Add("RequestPath", requestPath);
@@ -108,7 +111,7 @@ namespace Telemetry.Test
             }
 
             var expectedPath = "/openapi?url=/users?$filter(displayName eq '****')";
-            var expectedMessage = $"HTTP {httpMethod + expectedPath} responded {statusCode} in {elapsed} ms";
+            var expectedMessage = $"HTTP {httpMethod} {expectedPath} responded {statusCode} in {elapsed} ms";
 
             // Assert
             Assert.Equal(expectedPath, eventTelemetry.Properties["RequestPath"]);
@@ -123,7 +126,7 @@ namespace Telemetry.Test
             var statusCode = "200";
             var elapsed = "5000";
             var requestPath = "/openapi?url=/users?$filter(firstName eq 'Megan')";
-            var renderedMessage = $"HTTP {httpMethod + requestPath} responded {statusCode} in {elapsed} ms";
+            var renderedMessage = $"HTTP {httpMethod} {requestPath} responded {statusCode} in {elapsed} ms";
 
             var eventTelemetry = new EventTelemetry();
             eventTelemetry.Properties.Add("RequestPath", requestPath);
@@ -138,7 +141,7 @@ namespace Telemetry.Test
             }
 
             var expectedPath = "/openapi?url=/users?$filter(firstName eq '****')";
-            var expectedMessage = $"HTTP {httpMethod + expectedPath} responded {statusCode} in {elapsed} ms";
+            var expectedMessage = $"HTTP {httpMethod} {expectedPath} responded {statusCode} in {elapsed} ms";
 
             // Assert
             Assert.Equal(expectedPath, eventTelemetry.Properties["RequestPath"]);
