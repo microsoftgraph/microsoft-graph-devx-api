4c79965bbf9b411f1d0c6929e4bc545ac3f8839d
Irvine <irochand@microsoft.com>
2021-06-09 21:43:40 +0300

    Create UtilityConstants class that will have constants

---
+++ UtilityService/UtilityConstants.cs
@@ -0,0 +1,16 @@
+﻿// ------------------------------------------------------------------------------------------------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
+// ------------------------------------------------------------------------------------------------------------------------------------------------------
+
+namespace UtilityService
+{
+    /// <summary>
+    /// Provides reusable constants.
+    /// </summary>
+    public static class UtilityConstants
+    {
+        public const string TelemetryPropertyKey_Count = "Count";
+        public const string TelemetryPropertyKey_Permissions = "Permissions";
+        public const string TelemetryPropertyKey_Samples = "Samples";
+    }
+}
