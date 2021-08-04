4107527dd7500ba53a6b9024f8257a248955d83a
Maggie Kimani <maggiekim42@gmail.com>
2021-08-04 23:18:04 +0300

    Adds a Known Issues endpoint (#648)
    * Onboard the Known Issues web API
    
    * Move access token to config
    
    * Use constant for reusability
    
    * Add project reference
    
    * Add knownIssues test project
    
    * Add unit tests and a test config file
    
    * Add sample work item query data for testing
    
    * Create a mocked WorkItemTracking Http Client for testing
    
    * Refactor by decoupling the main QueryBugs() method for easier testing
    
    * Update interface with class methods
    
    * Update config settings and .sln
    
    * Remove unused file
    
    * Replace class with a record for immutability
    
    * Explicit variable declaration and refactor test method names for consistency
    
    * Make class static
    
    * Implement review feedback
    
    * Refactor code
    
    * Add trace telemetry
    
    * Add a known issues telemetry property
    
    * Copy file to output directory
    
    * Remove unnecessary config object and test files
    
    * Add trace telemetry
    
    * Remove config property
    
    * Defensive programming
    
    * Add known issues endpoint path operations to the OpenAPI yaml file
    
    * Update knownIssues path operations
    
    * Add settings test file and upgrade package
    
    * Use public interface properties for reusing in test project and eliminate the second constructor
    
    * Code cleanup
    
    * Clean up code
    
    * Remove whitespace and unnecessary using

---
+++ KnownIssuesService.Test/KnownIssuesServiceShould.cs
@@ -0,0 +1,87 @@
+// ------------------------------------------------------------------------------------------------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
+// ------------------------------------------------------------------------------------------------------------------------------------------------------
+
+using KnownIssuesService.Interfaces;
+using KnownIssuesService.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.TeamFoundation.WorkItemTracking.WebApi;
+using Microsoft.TeamFoundation.WorkItemTracking.WebApi.Models;
+using Moq;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace KnownIssuesService.Test
+{
+    public class KnownIssuesServiceShould
+    {
+        private readonly IKnownIssuesService _knownIssuesService;
+        private readonly WorkItemTrackingHttpClientMock _workItemTrackingHttpClientMock;
+        private readonly WorkItemTrackingHttpClient _workItemTrackingHttpClient;
+        private readonly Mock<Wiql> _wiqlTest;
+        private readonly IConfigurationRoot _configuration;
+
+        public KnownIssuesServiceShould()
+        {
+            _configuration = new ConfigurationBuilder()
+                            .AddJsonFile("appsettingstest.json")
+                            .Build();
+            _wiqlTest = new Mock<Wiql>();
+            _workItemTrackingHttpClientMock = new WorkItemTrackingHttpClientMock();
+            _workItemTrackingHttpClient = _workItemTrackingHttpClientMock.MockWorkItemTrackingHttpClient(_wiqlTest);
+            _knownIssuesService = new Services.KnownIssuesService(_configuration, wiql: _wiqlTest.Object, httpQueryClient: _workItemTrackingHttpClient);
+        }
+
+        [Fact]
+        public async Task GetQueryByWiql()
+        {
+            //Arrange
+            int expectedNoOfWorkItems = 5;
+
+            //Act
+            WorkItemQueryResult workItemQueryResult = await _knownIssuesService.GetQueryByWiqlAsync();
+            int actualNoOfWorkItems = workItemQueryResult.WorkItems.ToList().Count;
+
+            //Assert
+            Assert.Equal(expectedNoOfWorkItems, actualNoOfWorkItems);
+        }
+
+        [Fact]
+        public async Task GetWorkItemsQuery()
+        {
+            //Arrange
+            int expectedNoOfWorkItems = 4;
+
+            //Act
+            WorkItemQueryResult workItemQueryResult = await _knownIssuesService.GetQueryByWiqlAsync();
+            int[] ids = workItemQueryResult.WorkItems.Select(item => item.Id).ToArray();
+            List<WorkItem> items = await _knownIssuesService.GetWorkItemsQueryAsync(ids, workItemQueryResult);
+
+            //Assert
+            Assert.Equal(expectedNoOfWorkItems, items.Count);
+        }
+
+        [Fact]
+        public async Task QueryBugs()
+        {
+            //Arrange
+            int expectedNoOfWorkItems = 4;
+            KnownIssue contract = new KnownIssue()
+            {
+                Title = "Issue B",
+                State = "Active",
+                WorkLoadArea = "Notifications"
+            };
+            //Act
+            List<KnownIssue> items = await _knownIssuesService.QueryBugsAsync();
+
+            //Assert
+            Assert.Equal(expectedNoOfWorkItems, items.Count);
+            Assert.Equal(contract.Title, items[1].Title);
+            Assert.Equal(contract.State, items[1].State);
+            Assert.Equal(contract.WorkLoadArea, items[1].WorkLoadArea);
+        }
+    }
+}
---
+++ KnownIssuesService.Test/WorkItemTrackingHttpClientMock.cs
@@ -0,0 +1,44 @@
+﻿// ------------------------------------------------------------------------------------------------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
+// ------------------------------------------------------------------------------------------------------------------------------------------------------
+
+using Microsoft.TeamFoundation.WorkItemTracking.WebApi;
+using Microsoft.TeamFoundation.WorkItemTracking.WebApi.Models;
+using Moq;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace KnownIssuesService.Test
+{
+    /// <summary>
+    /// This class creates a mocked instance of a WorkItemTracking HttpClient for tracking work items
+    /// </summary>
+    public class WorkItemTrackingHttpClientMock
+    {
+        private readonly Mock<WorkItemTrackingHttpClient> _workItemTrackHttpClient;
+        private readonly WorkItemQueryResult _workItemQueryResult;
+
+        public WorkItemTrackingHttpClientMock()
+        {
+            _workItemTrackHttpClient = new Mock<WorkItemTrackingHttpClient>(null, null);
+            _workItemQueryResult = new WorkItemQueryResult();
+            _workItemQueryResult.WorkItems = WorkItemsStubData.GetWorkItemReferences();
+        }
+
+        /// <summary>
+        /// Creates a fake WorkItemTracking http client using stub test data
+        /// </summary>
+        /// <param name="wiql">A mocked instance of a wiql object.</param>
+        /// <returns> The mocked instance of a WorkItemTracking http client.</returns>
+        public WorkItemTrackingHttpClient MockWorkItemTrackingHttpClient(Mock<Wiql> wiql)
+        {
+            List<WorkItem> workItems = WorkItemsStubData.GetWorkItems();
+
+            _workItemTrackHttpClient.Setup(x => x.QueryByWiqlAsync(wiql.Object, null, 100, null, CancellationToken.None)).ReturnsAsync(_workItemQueryResult);
+            _workItemTrackHttpClient.Setup(x => x.GetWorkItemsAsync(
+                    new int[] { 9075, 9076, 9077, 9078, 9079 }, null, _workItemQueryResult.AsOf, null, null, null, CancellationToken.None)).ReturnsAsync(workItems);
+
+            return _workItemTrackHttpClient.Object;
+        }
+    }
+}
