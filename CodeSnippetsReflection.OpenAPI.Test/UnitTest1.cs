1c557594458fd2a934ba380d1ec5473dd4a879c1
Vincent Biret <vibiret@microsoft.com>
2021-08-25 13:40:21 +0000

    - adds unit test project to open api snippets generation
    Signed-off-by: GitHub <noreply@github.com>

---
+++ CodeSnippetsReflection.OpenAPI.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace CodeSnippetsReflection.OpenAPI.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
