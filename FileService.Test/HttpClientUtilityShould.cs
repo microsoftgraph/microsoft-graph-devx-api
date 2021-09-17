57b30503a65536733f5c19778514a08a190eab75
Maggie Kimani <maggiekim42@gmail.com>
2021-09-17 08:59:08 +0300

    Adds  HttClientUtility tests to increase code coverage

---
+++ FileService.Test/HttpClientUtilityShould.cs
@@ -0,0 +1,49 @@
+﻿// ------------------------------------------------------------------------------------------------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
+// ------------------------------------------------------------------------------------------------------------------------------------------------------
+
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using FileService.Services;
+using MockTestUtility;
+using Xunit;
+
+namespace FileService.Test
+{
+    public class HttpClientUtilityShould
+    {
+        private readonly HttpClient _httpClientMock;
+        private readonly HttpClientUtility _httpClientUtility;
+
+        public HttpClientUtilityShould()
+        {
+            _httpClientMock = new HttpClient(new MockHttpMessageHandler());
+            _httpClientUtility = new HttpClientUtility(_httpClientMock);
+        }
+
+        [Fact]
+        public async Task ReturnContentAsString()
+        {
+            HttpContent httpContent = new StringContent("Content as string");
+            var uri = "http://api/test";
+
+            HttpRequestMessage message = new HttpRequestMessage
+            {
+                RequestUri =new Uri(uri),
+                Content = httpContent
+            };
+
+            var content = await _httpClientUtility.ReadFromDocumentAsync(message);
+
+            Assert.Equal("Content as string", content);
+        }
+
+        [Fact]
+        public void ThrowArgumentNullExceptionIfRequestMessageIsNull()
+        {
+            // Act and Assert
+            Assert.Throws<ArgumentNullException>(() => _httpClientUtility.ReadFromDocumentAsync(null).GetAwaiter().GetResult());
+        }
+    }
+}
