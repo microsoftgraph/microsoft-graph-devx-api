354bdb62e2cb768eaa5e0c8594ca0a39295d880f
Maggie Kimani <maggiekim42@gmail.com>
2021-09-29 10:28:12 +0300

    Remove deprecated authentication handler configuration

--- GraphWebApi/Startup.cs
+++ GraphWebApi/Startup.cs
@@ -49,20 +49,6 @@ namespace GraphWebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddAuthentication(option =>
-            {
-                option.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-            })
-                   .AddJwtBearer(option =>
-                   {
-                       option.Authority = string.Format("{0}{1}", Configuration["AzureAd:Instance"], Configuration["AzureAd:TenantId"]);
-                       option.TokenValidationParameters = new TokenValidationParameters
-                       {
-                           ValidAudience = Configuration["AzureAd:Audience"],
-                           ValidIssuer = Configuration["AzureAd:Issuer"]
-                       };
-                   });
-
             #region AppInsights
 
             services.AddApplicationInsightsTelemetry(options =>
