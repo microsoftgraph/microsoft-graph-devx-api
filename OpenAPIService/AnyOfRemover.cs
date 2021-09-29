f27d934abd288487daa9e82fdd1e359fc70396fa
Irvine Sunday <irochand@microsoft.com>
2021-09-29 19:24:22 +0300

    Add AnyOf remover

--- OpenAPIService/AnyOfRemover.cs
+++ OpenAPIService/AnyOfRemover.cs
@@ -12,19 +12,48 @@ namespace OpenAPIService
             {
                 var newSchema = schema.AnyOf.FirstOrDefault();
                 schema.AnyOf = null;
-                if (newSchema != null)
+                //if (newSchema != null)
+                //{
+                //    if (newSchema.Reference != null)
+                //    {
+                //        schema.Reference = newSchema.Reference;
+                //        schema.UnresolvedReference = true;
+                //    }
+                //    else
+                //    {
+                //        schema.Type = newSchema.Type;
+                //    }
+                //}
+
+                schema = AddSchemaReference(newSchema);
+            }
+
+            if (schema.AllOf != null && schema.AllOf.Count > 0)
+            {
+                var newSchema = schema.AllOf.FirstOrDefault();
+                schema.AllOf = null;
+                schema = AddSchemaReference(newSchema);
+            }
+        }
+
+        private OpenApiSchema AddSchemaReference(OpenApiSchema newSchema)
+        {
+            OpenApiSchema schema = new();
+
+            if (newSchema != null)
+            {
+                if (newSchema.Reference != null)
                 {
-                    if (newSchema.Reference != null)
-                    {
-                        schema.Reference = newSchema.Reference;
-                        schema.UnresolvedReference = true;
-                    }
-                    else
-                    {
-                        schema.Type = newSchema.Type;
-                    }
+                    schema.Reference = newSchema.Reference;
+                    schema.UnresolvedReference = true;
+                }
+                else
+                {
+                    schema.Type = newSchema.Type;
                 }
             }
+
+            return schema;
         }
     }
 }
