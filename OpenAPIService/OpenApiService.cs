a6df394d2fc314e20df754abee0c82a05c3c9098
Irvine Sunday <irochand@microsoft.com>
2021-09-30 01:01:24 +0300

    Fix for EscapePoundCharacter

--- OpenAPIService/OpenApiService.cs
+++ OpenAPIService/OpenApiService.cs
@@ -742,7 +742,7 @@ namespace OpenAPIService
                 if (parentSchema.Properties.TryGetValue("description", out OpenApiSchema descriptionSchema))
                 {
                     // PowerShell uses ` to escape special characters
-                    descriptionSchema.Description = descriptionSchema.Description.Replace("<#>", "<#/>");
+                    descriptionSchema.Description = descriptionSchema.Description?.Replace("<#>", "<#/>");
                 }
             }
         }
