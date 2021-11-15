d21653436b22c76ed763a3ebb6b4a6017f8c2827
Irvine Sunday <40403681+irvinesunday@users.noreply.github.com>
2021-11-15 10:47:20 +0300

    Fixes release pipeline build failure (#783)
    * Remove dependency on fetching test data from another test class
    
    * Update tests
    
    * Remove test method dependency
    
    * Add more tests; remove unnecessary usings; update pagination logic
    
    * Address Sonarcloud suggestion
    
    Throw relevant exception ArgumentOutOfRangeException

--- MockTestUtility/MockHttpConstants.cs
+++ MockTestUtility/MockHttpConstants.cs
@@ -22,7 +22,7 @@ namespace MockTestUtility
                 "\"https://graph.office.net/en-us/graph/api/proxy\""
             },
             {
-                "https://graph.office.net/en-us/graph/api/proxy?url=https://graph.microsoft.com/v1.0/me/calendar?$whatif",
+                "https://graph.office.net/en-us/graph/api/proxy?url=https://graph.microsoft.com/v1.0/me/calendar/events?$whatif",
                 @"{
    ""Description"":""Runs main request and in case of NotFound, discovers the uri and runs the request again."",
    ""Uri"":""https://outlook.office365.com/api/gv1.0/Users('48d31887-5fad-4d73-a9f5-3c356e68a038%40dcd219dd-bc68-4b9b-bf0b-4a33a796be35')/messages?$whatif"",
