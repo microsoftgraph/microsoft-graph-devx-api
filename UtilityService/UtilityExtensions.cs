0b7f394b1df23d6c57ab2ab3846ca20349e6d736
Irvine Sunday <irochand@microsoft.com>
2022-03-17 16:04:52 +0300

    Add new extension method that resolves a string line breaks

--- UtilityService/UtilityExtensions.cs
+++ UtilityService/UtilityExtensions.cs
@@ -154,5 +154,18 @@ namespace UtilityService
 
             return string.Join(ForwardSlash, segments);
         }
+
+        /// <summary>
+        /// Change the input string's line breaks
+        /// </summary>
+        /// <param name="rawString">The raw input string</param>
+        /// <param name="newLine">The new line break.</param>
+        /// <returns>The changed string.</returns>
+        public static string ChangeLineBreaks(this string rawString, string newLine = "\n")
+        {
+            rawString = rawString.Trim('\n', '\r');
+            rawString = rawString.Replace("\r\n", newLine);
+            return rawString;
+        }
     }
 }
