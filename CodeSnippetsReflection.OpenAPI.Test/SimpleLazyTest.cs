4e787f8a3ece422e424628c00e815623c00e6883
Ronald K <43806892+rkodev@users.noreply.github.com>
2022-04-22 20:22:53 +0300

    Adds  a simple lazy object (#951)
    * Adds  asimple lazy initialization
    
    * Tests SimpleLazy with chaotic initalization

---
+++ CodeSnippetsReflection.OpenAPI.Test/SimpleLazyTest.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CodeSnippetsReflection.OpenAPI.Test
+{
+    public class SimpleLazyTest
+    {
+        private static Random random = new Random();
+
+        // return a random string or throw an exception
+        private String chaoticService()
+        {
+            if (random.Next() % 2 == 0 ) throw new Exception("Chaos just because");
+
+            Thread.Sleep(300);
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, 10)
+                .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
+        [Fact]
+        public async Task NoExceptionCaching()
+        {
+            var responseProvider = new SimpleLazy<String>(() => chaoticService());
+
+            String result = default;
+
+            Parallel.For(0, 15, t =>
+            {
+                for (int i = 0; i < 10; i++)
+                {
+
+                    if(result != default) // once value is set value should not be changed
+                    {
+                        if (result != responseProvider.Value)
+                        {
+                            Assert.Equal(result , responseProvider.Value);
+                        }
+                    }
+                    else
+                    {
+                        try
+                        {
+                            result = responseProvider.Value;
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.ToString());
+                        }
+                    }
+                }
+            });
+        }
+    }
+}
---
+++ CodeSnippetsReflection.OpenAPI/SimpleLazy.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeSnippetsReflection.OpenAPI
+{
+
+    /*
+     * A Simple lazy factory that substitues https://referencesource.microsoft.com/#mscorlib/system/Lazy.cs
+     * by disregarding any exceptions thrown by the value factory.
+     * This is a bypass for the dotnet issue https://github.com/dotnet/runtime/issues/27421 for lazy initialization without exception caching
+     * 
+     */
+    public class SimpleLazy<T> where T : class
+    {
+        private readonly Func<T> valueFactory;
+        private T instance;
+        private readonly object locker = new object();
+
+        public SimpleLazy(Func<T> valueFactory)
+        {
+            if (valueFactory == null)
+                throw new ArgumentNullException("valueFactory");
+
+            this.valueFactory = valueFactory;
+            this.instance = null;
+        }
+
+        public T Value
+        {
+            get
+            {
+                lock (locker)
+                    return instance ?? (instance = valueFactory());
+            }
+        }
+    }
+}
