f8e36dac2ecdfece9b2a7d30028633ff737331bd
silaskenneth <silaskenneth1@gmail.com>
2022-06-28 12:47:06 +0300

    Update CodeSnippetsReflection.OpenAPI/LanguageGenerators/PhpGenerator.cs
    Co-authored-by: Vincent Biret <vibiret@microsoft.com>

--- CodeSnippetsReflection.OpenAPI/LanguageGenerators/PhpGenerator.cs
+++ CodeSnippetsReflection.OpenAPI/LanguageGenerators/PhpGenerator.cs
@@ -25,7 +25,7 @@ public class PhpGenerator : ILanguageGenerator<SnippetModel, OpenApiUrlTreeNode>
         snippetBuilder.Append(requestPayload);
         var responseAssignment = "$result = ";
         // have a return type if we have a response schema that is not an error
-        if (snippetModel.ResponseSchema == null || (snippetModel.ResponseSchema.Properties.Count == 1 && snippetModel.ResponseSchema.Properties.First().Key.Equals("error",StringComparison.OrdinalIgnoreCase)))
+        if (snippetModel.ResponseSchema == null || (snippetModel.ResponseSchema.Properties.Any() && snippetModel.ResponseSchema.Properties.First(x => x.Key.Equals("error",StringComparison.OrdinalIgnoreCase)))
             responseAssignment = string.Empty;
         if (string.IsNullOrEmpty(responseAssignment) && !string.IsNullOrEmpty(snippetModel.ResponseVariableName)) 
             responseAssignment = "$result = ";
