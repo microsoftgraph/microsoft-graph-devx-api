efeddf98ed5614a3f458a63e6f88303395ce9c52
Peter Ombwa <peombwa@microsoft.com>
2022-09-19 08:36:33 -0700

    Remove unnecessary usings.

--- OpenAPIService/AnyOfOneOfRemover.cs
+++ OpenAPIService/AnyOfOneOfRemover.cs
@@ -1,8 +1,9 @@
-﻿using Microsoft.OpenApi.Any;
+﻿// ------------------------------------------------------------------------------------------------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
+// ------------------------------------------------------------------------------------------------------------------------------------------------------
+
 using Microsoft.OpenApi.Models;
 using Microsoft.OpenApi.Services;
-using System.Collections.Generic;
-using System;
 using System.Linq;
 
 namespace OpenAPIService
