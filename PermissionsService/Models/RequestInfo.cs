e06151f78d2083d47b52d079643e7acb629b20d3
Millicent Achieng <achieng.milli@gmail.com>
2023-04-18 20:11:19 +0300

    When fetching least privilege permissions for list of request URLs, get least privilege scopes for the set and not for individual request URLs

---
+++ PermissionsService/Models/RequestInfo.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace PermissionsService.Models
+{
+    public class RequestInfo
+    {
+        [JsonProperty(PropertyName = "requestUrl")]
+        public string RequestUrl
+        {
+            get; set;
+        }
+
+        [JsonProperty(PropertyName = "method")]
+        public string HttpMethod
+        {
+            get; set;
+        }
+    }
+}
