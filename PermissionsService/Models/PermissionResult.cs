a9db951d7c4b7697712dfc8f4b93f97b0712aec8
Millicent Achieng <achieng.milli@gmail.com>
2023-05-23 23:08:54 +0300

    Add licence info to files

--- PermissionsService/Models/PermissionResult.cs
+++ PermissionsService/Models/PermissionResult.cs
@@ -1,4 +1,8 @@
-﻿using System.Collections.Generic;
+﻿// -------------------------------------------------------------------------------------------------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
+// -------------------------------------------------------------------------------------------------------------------------------------------------------
+
+using System.Collections.Generic;
 using Newtonsoft.Json;
 
 namespace PermissionsService.Models
--- PermissionsService/Models/ScopeType.cs
+++ PermissionsService/Models/ScopeType.cs
@@ -1,4 +1,8 @@
-﻿namespace PermissionsService.Models
+﻿// -------------------------------------------------------------------------------------------------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
+// -------------------------------------------------------------------------------------------------------------------------------------------------------
+
+namespace PermissionsService.Models
 {
     /// <summary>
     /// Defines permission schemes
