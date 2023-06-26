b1ed167fa5870d3855f6f1655c50141a2aa47271
Eastman <andrueastman@users.noreply.github.com>
2023-06-26 17:50:15 +0300

    Aligns to kiota generation (#1632)

--- CodeSnippetsReflection.OpenAPI.Test/CSharpGeneratorTests.cs
+++ CodeSnippetsReflection.OpenAPI.Test/CSharpGeneratorTests.cs
@@ -731,6 +731,69 @@ namespace CodeSnippetsReflection.OpenAPI.Test
             Assert.Contains("new FileAttachment", result);// Individual items are derived types
             Assert.Contains("ContentBytes = Convert.FromBase64String(\"SGVsbG8gV29ybGQh\"),", result);
         }
+        [Fact]
+        public async Task GeneratesPropertiesWithSpecialCharacters() {
+            var sampleJson = @"{
+              ""@odata.type"": ""#microsoft.graph.managedIOSLobApp"",
+              ""displayName"": ""Display Name value"",
+              ""description"": ""Description value"",
+              ""publisher"": ""Publisher value"",
+              ""largeIcon"": {
+                ""@odata.type"": ""microsoft.graph.mimeContent"",
+                ""type"": ""Type value"",
+                ""value"": ""dmFsdWU=""
+              },
+              ""isFeatured"": true,
+              ""privacyInformationUrl"": ""https://example.com/privacyInformationUrl/"",
+              ""informationUrl"": ""https://example.com/informationUrl/"",
+              ""owner"": ""Owner value"",
+              ""developer"": ""Developer value"",
+              ""notes"": ""Notes value"",
+              ""uploadState"": 11,
+              ""publishingState"": ""processing"",
+              ""isAssigned"": true,
+              ""roleScopeTagIds"": [
+                ""Role Scope Tag Ids value""
+              ],
+              ""dependentAppCount"": 1,
+              ""supersedingAppCount"": 3,
+              ""supersededAppCount"": 2,
+              ""appAvailability"": ""lineOfBusiness"",
+              ""version"": ""Version value"",
+              ""committedContentVersion"": ""Committed Content Version value"",
+              ""fileName"": ""File Name value"",
+              ""size"": 4,
+              ""bundleId"": ""Bundle Id value"",
+              ""applicableDeviceType"": {
+                ""@odata.type"": ""microsoft.graph.iosDeviceType"",
+                ""iPad"": true,
+                ""iPhoneAndIPod"": true
+              },
+              ""minimumSupportedOperatingSystem"": {
+                ""@odata.type"": ""microsoft.graph.iosMinimumOperatingSystem"",
+                ""v8_0"": true,
+                ""v9_0"": true,
+                ""v10_0"": true,
+                ""v11_0"": true,
+                ""v12_0"": true,
+                ""v13_0"": true,
+                ""v14_0"": true,
+                ""v15_0"": true,
+                ""v16_0"": true
+              },
+              ""expirationDateTime"": ""2016-12-31T23:57:57.2481234-08:00"",
+              ""versionNumber"": ""Version Number value"",
+              ""buildNumber"": ""Build Number value"",
+              ""identityVersion"": ""Identity Version value""
+            }";
+            using var requestPayload = new HttpRequestMessage(HttpMethod.Patch, $"{ServiceRootBetaUrl}/deviceAppManagement/mobileApps/{{mobileAppId}}"){
+                Content = new StringContent(sampleJson, Encoding.UTF8, "application/json")
+            };
+            var snippetModel = new SnippetModel(requestPayload, ServiceRootUrl, await GetV1SnippetMetadata());
+            var result = _generator.GenerateCodeSnippet(snippetModel);
+            Assert.Contains("MinimumSupportedOperatingSystem = new IosMinimumOperatingSystem", result);
+            Assert.Contains("V80 = true,", result);//Assert that the property was pascal cased
+        }
         
         [Fact]
         public async Task CorrectlyHandlesTypeFromInUrl()
