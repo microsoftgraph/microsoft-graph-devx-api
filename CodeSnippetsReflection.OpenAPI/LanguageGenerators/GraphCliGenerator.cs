d977de3288911b81b0d66ad2a6af40e8e26511e9
Timothy Wamalwa <timwamalwa@gmail.com>
2023-06-28 21:31:13 +0300

    Added time limit to the regex

--- CodeSnippetsReflection.OpenAPI/LanguageGenerators/GraphCliGenerator.cs
+++ CodeSnippetsReflection.OpenAPI/LanguageGenerators/GraphCliGenerator.cs
@@ -225,7 +225,7 @@ public partial class GraphCliGenerator : ILanguageGenerator<SnippetModel, OpenAp
             {
                 string pattern = "\\?\\$\\w*=";
                 string[] splittedQueryString = Regex.Split(snippetModel.QueryString, pattern, RegexOptions.Compiled, TimeSpan.FromSeconds(5));
-                var match = Regex.Match(snippetModel.QueryString, pattern);
+                var match = Regex.Match(snippetModel.QueryString, pattern, RegexOptions.Compiled, TimeSpan.FromSeconds(5));
                 string queryOption = match.Groups[0].Value.Replace("?", "").Replace("=", "");
                 string queryOptionFunction = splittedQueryString[1].Replace("$", "`$");
                 queryOptionFunction = queryOptionFunction.Replace(queryOptionFunction, "\"" + queryOptionFunction + "\"");
