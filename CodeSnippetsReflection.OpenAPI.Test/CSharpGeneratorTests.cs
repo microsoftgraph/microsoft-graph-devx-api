19657251379ce5771e431e6522eec5a691db10df
Andrew Omondi <andrew.omondi@microsoft.com>
2023-07-05 13:10:15 +0300

    Revert

--- CodeSnippetsReflection.OpenAPI.Test/CSharpGeneratorTests.cs
+++ CodeSnippetsReflection.OpenAPI.Test/CSharpGeneratorTests.cs
@@ -845,33 +845,6 @@ namespace CodeSnippetsReflection.OpenAPI.Test
         }
         
         [Fact]
-        public async Task GeneratesCorrectStringsIfEscaped() {
-            var sampleJson = @"{
-                ""subject"": null,
-                ""body"": {
-                    ""contentType"": ""html"",
-                    ""content"": ""<attachment id=\""74d20c7f34aa4a7fb74e2b30004247c5\""></attachment>""
-                },
-                ""attachments"": [
-                    {
-                        ""id"": ""74d20c7f34aa4a7fb74e2b30004247c5"",
-                        ""contentType"": ""application/vnd.microsoft.card.thumbnail"",
-                        ""contentUrl"": null,
-                        ""content"": ""{\r\n  \""title\"": \""This is an example of posting a card\"",\r\n  \""subtitle\"": \""<h3>This is the subtitle</h3>\"",\r\n  \""text\"": \""Here is some body text. <br>\\r\\nAnd a <a href=\\\""http://microsoft.com/\\\"">hyperlink</a>. <br>\\r\\nAnd below that is some buttons:\"",\r\n  \""buttons\"": [\r\n    {\r\n      \""type\"": \""messageBack\"",\r\n      \""title\"": \""Login to FakeBot\"",\r\n      \""text\"": \""login\"",\r\n      \""displayText\"": \""login\"",\r\n      \""value\"": \""login\""\r\n    }\r\n  ]\r\n}"",
-                        ""name"": null,
-                        ""thumbnailUrl"": null
-                    }
-                ]
-            }";
-            using var requestPayload = new HttpRequestMessage(HttpMethod.Post, $"{ServiceRootUrl}//teams/fbe2bf47-16c8-47cf-b4a5-4b9b187c508b/channels/19:4a95f7d8db4c4e7fae857bcebe0623e6@thread.tacv2/messages"){
-                Content = new StringContent(sampleJson, Encoding.UTF8, "application/json")
-            };
-            var snippetModel = new SnippetModel(requestPayload, ServiceRootUrl, await GetV1SnippetMetadata());
-            var result = _generator.GenerateCodeSnippet(snippetModel);
-            Assert.Contains("new List<Microsoft.Graph.Models.IdentityGovernance.TaskObject>", result);//Assert the type is escaped in the collection initializzer.
-        }
-        
-        [Fact]
         public async Task CorrectlyHandlesTypeFromInUrl()
         {
             using var requestPayload = new HttpRequestMessage(HttpMethod.Get, $"{ServiceRootUrl}/me/mailFolders/?includehiddenfolders=true");
