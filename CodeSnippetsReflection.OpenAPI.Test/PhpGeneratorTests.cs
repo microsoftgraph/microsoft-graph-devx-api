3131699b29fc65a3a96094e253ed0708b55b8060
silaskenneth <silaskenneth1@gmail.com>
2023-07-24 18:18:56 +0300

    Fix issue with representing UUIDs. (#1680)
    * Fix issue with representing UUIDs.
    
    * Remove unused imports.

--- CodeSnippetsReflection.OpenAPI.Test/PhpGeneratorTests.cs
+++ CodeSnippetsReflection.OpenAPI.Test/PhpGeneratorTests.cs
@@ -660,4 +660,104 @@ public class PhpGeneratorTests : OpenApiSnippetGeneratorTestBase
         Assert.Contains("'scope' => 				[", result);
         Assert.Contains("'trigger' => 				[", result);
     }
+
+    [Fact]
+    public async Task GenerateForGuid()
+    {
+        const string url = "/communications/calls/logTeleconferenceDeviceQuality";
+        const string body = @"
+        {
+          ""quality"": {
+            ""@odata.type"": ""#microsoft.graph.teleconferenceDeviceQuality"",
+            ""callChainId"": ""0622673d-9f69-49b3-9d4f-5ec64f42ecce"",
+            ""participantId"": ""ea078406-b5d4-4d3c-b85e-90103dcec7f6"",
+            ""mediaLegId"": ""bd9ee398-4b9d-42c7-8b8d-4e8efad9435f"",
+            ""deviceName"": ""TestAgent"",
+            ""deviceDescription"": ""TestDescription"",
+            ""mediaQualityList"": [
+              {
+                ""@odata.type"": ""#microsoft.graph.teleconferenceDeviceAudioQuality"",
+                ""channelIndex"": 1,
+                ""mediaDuration"": ""PT20M"",
+                ""networkLinkSpeedInBytes"": 13000,
+                ""localIPAddress"": ""127.0.0.1"",
+                ""localPort"": 6300,
+                ""remoteIPAddress"": ""102.1.1.101"",
+                ""remotePort"": 6301,
+                ""inboundPackets"": 5500,
+                ""outboundPackets"": 5400,
+                ""averageInboundPacketLossRateInPercentage"": 0.01,
+                ""averageOutboundPacketLossRateInPercentage"": 0.02,
+                ""maximumInboundPacketLossRateInPercentage"": 0.05,
+                ""maximumOutboundPacketLossRateInPercentage"": 0.06,
+                ""averageInboundRoundTripDelay"": ""PT0.03S"",
+                ""averageOutboundRoundTripDelay"": ""PT0.04S"",
+                ""maximumInboundRoundTripDelay"": ""PT0.13S"",
+                ""maximumOutboundRoundTripDelay"": ""PT0.14S"",
+                ""averageInboundJitter"": ""PT0.01S"",
+                ""averageOutboundJitter"": ""PT0.015S"",
+                ""maximumInboundJitter"": ""PT0.023S"",
+                ""maximumOutboundJitter"": ""PT0.024S""
+              },
+              {
+                ""@odata.type"": ""#microsoft.graph.teleconferenceDeviceVideoQuality"",
+                ""channelIndex"": 1,
+                ""mediaDuration"": ""PT20M"",
+                ""networkLinkSpeedInBytes"": 13000,
+                ""localIPAddress"": ""127.0.0.1"",
+                ""localPort"": 6300,
+                ""remoteIPAddress"": ""102.1.1.101"",
+                ""remotePort"": 6301,
+                ""inboundPackets"": 5500,
+                ""outboundPackets"": 5400,
+                ""averageInboundPacketLossRateInPercentage"": 0.01,
+                ""averageOutboundPacketLossRateInPercentage"": 0.02,
+                ""maximumInboundPacketLossRateInPercentage"": 0.05,
+                ""maximumOutboundPacketLossRateInPercentage"": 0.06,
+                ""averageInboundRoundTripDelay"": ""PT0.03S"",
+                ""averageOutboundRoundTripDelay"": ""PT0.04S"",
+                ""maximumInboundRoundTripDelay"": ""PT0.13S"",
+                ""maximumOutboundRoundTripDelay"": ""PT0.14S"",
+                ""averageInboundJitter"": ""PT0.01S"",
+                ""averageOutboundJitter"": ""PT0.015S"",
+                ""maximumInboundJitter"": ""PT0.023S"",
+                ""maximumOutboundJitter"": ""PT0.024S""
+              },
+              {
+                ""@odata.type"": ""#microsoft.graph.teleconferenceDeviceScreenSharingQuality"",
+                ""channelIndex"": 1,
+                ""mediaDuration"": ""PT20M"",
+                ""networkLinkSpeedInBytes"": 13000,
+                ""localIPAddress"": ""127.0.0.1"",
+                ""localPort"": 6300,
+                ""remoteIPAddress"": ""102.1.1.101"",
+                ""remotePort"": 6301,
+                ""inboundPackets"": 5500,
+                ""outboundPackets"": 5400,
+                ""averageInboundPacketLossRateInPercentage"": 0.01,
+                ""averageOutboundPacketLossRateInPercentage"": 0.02,
+                ""maximumInboundPacketLossRateInPercentage"": 0.05,
+                ""maximumOutboundPacketLossRateInPercentage"": 0.06,
+                ""averageInboundRoundTripDelay"": ""PT0.03S"",
+                ""averageOutboundRoundTripDelay"": ""PT0.04S"",
+                ""maximumInboundRoundTripDelay"": ""PT0.13S"",
+                ""maximumOutboundRoundTripDelay"": ""PT0.14S"",
+                ""averageInboundJitter"": ""PT0.01S"",
+                ""averageOutboundJitter"": ""PT0.015S"",
+                ""maximumInboundJitter"": ""PT0.023S"",
+                ""maximumOutboundJitter"": ""PT0.024S""
+              }
+            ]
+          }
+        }";
+        using var requestPayload =
+            new HttpRequestMessage(HttpMethod.Post, $"{ServiceRootUrl}{url}")
+            {
+                Content = new StringContent(body, Encoding.UTF8, "application/json")
+            };
+        var snippetModel = new SnippetModel(requestPayload, ServiceRootUrl, await GetV1SnippetMetadata());
+        var result = _generator.GenerateCodeSnippet(snippetModel);
+        Assert.Contains("new \\DateInterval(", result);
+        Assert.Contains("->setCallChainId('0622673d-9f69-49b3-9d4f-5ec64f42ecce')", result);
+    }
 }
