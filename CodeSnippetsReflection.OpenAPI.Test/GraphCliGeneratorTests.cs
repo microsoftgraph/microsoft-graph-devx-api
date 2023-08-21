01178339ba3d698b4fec24413248525795cdb882
Timothy Wamalwa <timwamalwa@gmail.com>
2023-08-21 19:24:05 +0300

    rectified failing test

--- CodeSnippetsReflection.OpenAPI.Test/GraphCliGeneratorTests.cs
+++ CodeSnippetsReflection.OpenAPI.Test/GraphCliGeneratorTests.cs
@@ -523,6 +523,6 @@ public class GraphCliGeneratorTests : OpenApiSnippetGeneratorTestBase
         var result = _generator.GenerateCodeSnippet(snippetModel);
 
         // Then
-        Assert.Equal(notice + Environment.NewLine + "mgc teams members list --team-id {team-id} --filter \"(microsoft.graph.aadUserConversationMember/displayName eq 'Harry Johnson' or microsoft.graph.aadUserConversationMember/email eq 'admin@M365x987948.OnMicrosoft.com')\"", result);
+        Assert.Equal(notice + Environment.NewLine + "mgc teams members list --team-id {team-id} --filter \"(microsoft.graph.aadUserConversationMember/displayName%20eq%20'Harry%20Johnson'%20or%20microsoft.graph.aadUserConversationMember/email%20eq%20'admin@M365x987948.OnMicrosoft.com')\"", result);
     }
 }
