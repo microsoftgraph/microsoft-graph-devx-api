22315ed7c40f2409bb4479bd9d6e00d66d71c1d5
ramsessanchez <63934382+ramsessanchez@users.noreply.github.com>
2023-09-22 12:34:17 -0700

    code review comments

--- CodeSnippetsReflection.OpenAPI.Test/JavaGeneratorTests.cs
+++ CodeSnippetsReflection.OpenAPI.Test/JavaGeneratorTests.cs
@@ -565,8 +565,7 @@ namespace CodeSnippetsReflection.OpenAPI.Test
         {
             var bodyContent = @"{
                 ""clientContext"": ""clientContext-value"",
-                ""status"": ""notRecorDing | recording , failed""
-            }";
+                ""status"": ""notRecorDing | recording , failed""}"; //Ensure enums are split by comma or pipe
 
             using var requestPayload = new HttpRequestMessage(HttpMethod.Post, $"{ServiceRootUrl}/communications/calls/{{id}}/updateRecordingStatus")
             {
@@ -882,58 +881,7 @@ namespace CodeSnippetsReflection.OpenAPI.Test
             var snippetModel = new SnippetModel(requestPayload, ServiceRootUrl, await GetV1SnippetMetadata());
             var result = _generator.GenerateCodeSnippet(snippetModel);
             Assert.Contains("IosMinimumOperatingSystem minimumSupportedOperatingSystem = new IosMinimumOperatingSystem", result);
-            Assert.Contains("setV80(true)", result);//Assert that the property was pascal cased
-        }
-
-        [Fact]
-        public async Task GeneratesCorrectTypeInCollectionInitializer()
-        {
-            var sampleJson = @"{
-                ""workflow"":{
-                    ""category"": ""joiner"",
-                    ""description"": ""Configure new hire tasks for onboarding employees on their first day"",
-                    ""displayName"": ""Global onboard new hire employee"",
-                    ""isEnabled"": true,
-                    ""isSchedulingEnabled"": false,
-                    ""executionConditions"": {
-                        ""@odata.type"": ""#microsoft.graph.identityGovernance.triggerAndScopeBasedConditions"",
-                        ""scope"": {
-                            ""@odata.type"": ""#microsoft.graph.identityGovernance.ruleBasedSubjectSet"",
-                            ""rule"": ""(department eq 'Marketing')""
-                        },
-                        ""trigger"": {
-                            ""@odata.type"": ""#microsoft.graph.identityGovernance.timeBasedAttributeTrigger"",
-                            ""timeBasedAttribute"": ""employeeHireDate"",
-                            ""offsetInDays"": 1
-                        }
-                    },
-                    ""tasks"": [
-                        {
-                            ""continueOnError"": false,
-                            ""description"": ""Enable user account in the directory"",
-                            ""displayName"": ""Enable User Account"",
-                            ""isEnabled"": true,
-                            ""taskDefinitionId"": ""6fc52c9d-398b-4305-9763-15f42c1676fc"",
-                            ""arguments"": []
-                        },
-                        {
-                            ""continueOnError"": false,
-                            ""description"": ""Send welcome email to new hire"",
-                            ""displayName"": ""Send Welcome Email"",
-                            ""isEnabled"": true,
-                            ""taskDefinitionId"": ""70b29d51-b59a-4773-9280-8841dfd3f2ea"",
-                            ""arguments"": []
-                        }
-                    ]
-                }
-            }";
-            using var requestPayload = new HttpRequestMessage(HttpMethod.Post, $"{ServiceRootUrl}/identityGovernance/lifecycleWorkflows/workflows/{{workflowId}}/createNewVersion")
-            {
-                Content = new StringContent(sampleJson, Encoding.UTF8, "application/json")
-            };
-            var snippetModel = new SnippetModel(requestPayload, ServiceRootUrl, await GetV1SnippetMetadata());
-            var result = _generator.GenerateCodeSnippet(snippetModel);
-            Assert.Contains("new LinkedList<com.microsoft.graph.models.identitygovernance.Task>", result);//Assert the type is escaped in the collection initializzer.
+            Assert.Contains("setV80(true)", result); //assert property name is pascal case after the 'set' portion
         }
 
         [Fact]
