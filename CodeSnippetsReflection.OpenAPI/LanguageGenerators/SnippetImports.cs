ec63d58bc39b0aba5604d9013051ac54a0c1cbab
shemogumbe <shemogumbe@microsoft.com>
2024-01-19 02:40:41 +0300

    multiple loops to get nested models

--- CodeSnippetsReflection.OpenAPI/LanguageGenerators/SnippetImports.cs
+++ CodeSnippetsReflection.OpenAPI/LanguageGenerators/SnippetImports.cs
@@ -9,16 +9,52 @@ using Microsoft.OpenApi.Services;
 namespace CodeSnippetsReflection.OpenAPI.ModelGraph;
 
 public class ImportsGenerator{
-    public List<Dictionary<string, object>> imports { get; set; } = new();
-    public List<Dictionary<string, object>> GenerateImportTemplates(SnippetModel snippetModel){
+    public List<Dictionary<string, string>> imports { get; set; } = new();
+    public List<Dictionary<string, string>> GenerateImportTemplates(SnippetModel snippetModel){
         var codeGraph = new SnippetCodeGraph(snippetModel);
-        foreach ( var child in codeGraph.Body.Children){
-            var import = new Dictionary<string, object>();
-            import.Add("Name", child.Name);
-            import.Add("TypeDefinition", child.TypeDefinition);
-            import.Add("NameSpace", child.NamespaceName);
+        if(codeGraph.Body.NamespaceName != null){
+            var import = new Dictionary<string, string>();
+            import.Add("Name", codeGraph.Body.Name);
+            import.Add("TypeDefinition", codeGraph.Body.TypeDefinition);
+            import.Add("NamespaceName", codeGraph.Body.NamespaceName);
             imports.Append(import);
-            
+        }
+        if (codeGraph.Body.Children.Count > 0){
+            foreach ( var child in codeGraph.Body.Children)
+            {
+                if(child.NamespaceName != null){
+                    var childimport = new Dictionary<string, string>();
+                    childimport.Add("Name", child.Name);
+                    childimport.Add("TypeDefinition", child.TypeDefinition);
+                    childimport.Add("NamespaceName", child.NamespaceName);
+                    imports.Append(childimport);
+                }
+                if (child.Children.Count > 0){
+                    var grandchildimport = new Dictionary<string, string>();
+                    foreach ( var grandchild in child.Children)
+                    {
+                        if(grandchild.NamespaceName != null){
+                            grandchildimport.Add("Name", grandchild.Name);
+                            grandchildimport.Add("TypeDefinition", grandchild.TypeDefinition);
+                            grandchildimport.Add("NamespaceName", grandchild.NamespaceName);
+                            imports.Append(grandchildimport);
+                        }
+                        if(grandchild.Children != null){
+                            var grandchild2import = new Dictionary<string, string>();
+                            foreach ( var grandchild2 in grandchild.Children)
+                            {
+                                if(grandchild2.NamespaceName != null){
+                                    grandchildimport.Add("Name", grandchild2.Name);
+                                    grandchildimport.Add("TypeDefinition", grandchild2.TypeDefinition);
+                                    grandchildimport.Add("NamespaceName", grandchild2.NamespaceName);
+                                    imports.Append(grandchildimport);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        
         }
         return imports;
     }
