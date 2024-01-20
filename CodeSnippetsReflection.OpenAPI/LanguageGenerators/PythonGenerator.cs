8824224146b0d40b2fdcd97d11028547ae23dfaa
shemogumbe <shemogumbe@microsoft.com>
2024-01-20 23:05:52 +0300

    Include python filename to import path for request body

--- CodeSnippetsReflection.OpenAPI/LanguageGenerators/PythonGenerator.cs
+++ CodeSnippetsReflection.OpenAPI/LanguageGenerators/PythonGenerator.cs
@@ -89,9 +89,7 @@ namespace CodeSnippetsReflection.OpenAPI.LanguageGenerators
                     snippetImports.Add($"{modelImportPrefix}.{import["Name"].ToSnakeCase()} import {import["Name"]}");
                 }
                 if(import.ContainsKey("NamespaceName") & !import["NamespaceName"].Contains("models")){
-                    // snippetImports.Add($"{requestBuilderImportPrefix}.{import["NamespaceName"].ToSnakeCase()}.{import["TypeDefinition"]+"_request_builder"} import {import["Name"]}");
-                    snippetImports.Add($"{requestBuilderImportPrefix}.{string.Join(".", import["NamespaceName"].Split('.').Select((s, i) => i == import["NamespaceName"].Split('.').Length - 1 ? s.ToSnakeCase() : s.ToLowerInvariant()))} import {import["Name"]}");
-                    
+                    snippetImports.Add($"{requestBuilderImportPrefix}.{string.Join(".", import["NamespaceName"].Split('.').Select((s, i) => i == import["NamespaceName"].Split('.').Length - 1 ? s.ToSnakeCase() : s.ToLowerInvariant()))}.{import["Name"].ToSnakeCase()} import {import["Name"]}");                    
                     //import["NamespaceName"].EndsWith("RequestBuilder") ||
                 }
 
