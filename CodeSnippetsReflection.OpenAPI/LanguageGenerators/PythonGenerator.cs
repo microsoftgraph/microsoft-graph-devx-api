e8bb76c4af91600bb7fdaa3771ec3f17a4578b8a
shemogumbe <shemogumbe@microsoft.com>
2024-01-21 22:49:15 +0300

    add request builder even if the snippet contains a model

--- CodeSnippetsReflection.OpenAPI/LanguageGenerators/PythonGenerator.cs
+++ CodeSnippetsReflection.OpenAPI/LanguageGenerators/PythonGenerator.cs
@@ -99,6 +99,10 @@ namespace CodeSnippetsReflection.OpenAPI.LanguageGenerators
                     var className = import["NamespaceName"].Split('.').Last();
                     var requestBuilderClassName = $"{className}RequestBuilder";
                     snippetImports.Add($"{requestBuilderImportPrefix}.{string.Join(".", import["NamespaceName"].Split('.').Select((s, i) => i == import["NamespaceName"].Split('.').Length - 1 ? s.ToSnakeCase() : s.ToLowerInvariant()))}.{import["RequestBuilderName"].ToSnakeCase()} import {requestBuilderClassName}");                    
+                }else if(import["RequestBuilderName"] != null){
+                  var className = import["NamespaceName"].Split('.').Last();
+                    var requestBuilderClassName = $"{className}RequestBuilder";
+                    snippetImports.Add($"{requestBuilderImportPrefix}.{string.Join(".", import["NamespaceName"].Split('.').Select((s, i) => i == import["NamespaceName"].Split('.').Length - 1 ? s.ToSnakeCase() : s.ToLowerInvariant()))}.{import["RequestBuilderName"].ToSnakeCase()} import {requestBuilderClassName}");                      
                 }
                 
             }
