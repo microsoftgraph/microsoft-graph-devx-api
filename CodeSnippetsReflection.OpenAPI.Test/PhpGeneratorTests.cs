3e9d94770f333b763514ac37013f445ea895e00a
shemogumbe <shemogumbe@microsoft.com>
2024-03-25 12:22:03 +0300

    fix failing tests

--- CodeSnippetsReflection.OpenAPI.Test/PhpGeneratorTests.cs
+++ CodeSnippetsReflection.OpenAPI.Test/PhpGeneratorTests.cs
@@ -544,6 +544,74 @@ public class PhpGeneratorTests : OpenApiSnippetGeneratorTestBase
         Assert.Contains("'@odata.type' => '#microsoft.graph.identity',", result);
         Assert.Contains("'id' => '+12345678901',", result);
     }
+    
+    [Fact]
+    public async Task GeneratesRequestBuilderImports()
+    {
+        using var requestPayload = new HttpRequestMessage(HttpMethod.Get, $"{ServiceRootUrl}/me/calendar/events?$filter=startsWith(subject,'All')");
+        var snippetModel = new SnippetModel(requestPayload, ServiceRootUrl, await GetV1SnippetMetadata());
+        var result = _generator.GenerateCodeSnippet(snippetModel);
+        Assert.Contains("use Microsoft\\Graph\\GraphServiceClient;", result);
+        Assert.Contains("use Microsoft\\Graph\\Generated\\Users\\Item\\Calendar\\Events\\EventsRequestBuilderGetRequestConfiguration;", result);
+    }
+
+    
+    [Fact]
+    public async Task GenerateModelImports(){
+        var bodyContent = @"{
+            ""displayName"":  ""New display name""
+            }";
+        using var requestPayload = new HttpRequestMessage(HttpMethod.Patch, $"{ServiceRootUrl}/applications/{{id}}")
+        {
+            Content = new StringContent(bodyContent, Encoding.UTF8, "application/json")
+        };
+        var snippetModel = new SnippetModel(requestPayload, ServiceRootUrl, await GetV1SnippetMetadata());
+        var result = _generator.GenerateCodeSnippet(snippetModel);
+        Assert.Contains("use Microsoft\\Graph\\GraphServiceClient;", result);
+        Assert.Contains("use Microsoft\\Graph\\Generated\\Models\\Application;", result);
+
+    }
+    
+    [Fact]
+    public async Task GenerateComplexModelImports(){
+        var bodyContent = @"{
+            ""subject"": ""Annual review"",
+            ""body"": {
+                ""contentType"": ""HTML"",
+                ""content"": ""You should be proud!""
+            },
+            ""toRecipients"": [
+                {
+                ""emailAddress"": {
+                    ""address"": ""rufus@contoso.com""
+                }
+                }
+            ],
+            ""extensions"": [
+                {
+                ""@odata.type"": ""microsoft.graph.openTypeExtension"",
+                ""extensionName"": ""Com.Contoso.Referral"",
+                ""companyName"": ""Wingtip Toys"",
+                ""expirationDate"": ""2015-12-30T11:00:00.000Z"",
+                ""dealValue"": 10000
+                }
+            ]
+        }";
+        using var requestPayload = new HttpRequestMessage(HttpMethod.Post, $"{ServiceRootUrl}/me/messages/")
+        {
+            Content = new StringContent(bodyContent, Encoding.UTF8, "application/json")
+        };
+        var snippetModel = new SnippetModel(requestPayload, ServiceRootUrl, await GetV1SnippetMetadata());
+        var result = _generator.GenerateCodeSnippet(snippetModel);
+        Assert.Contains("use Microsoft\\Graph\\GraphServiceClient;", result);
+        Assert.Contains("use Microsoft\\Graph\\Generated\\Models\\Message;", result);
+        Assert.Contains("use Microsoft\\Graph\\Generated\\Models\\ItemBody;", result);
+        Assert.Contains("use Microsoft\\Graph\\Generated\\Models\\Recipient;", result);
+        Assert.Contains("use Microsoft\\Graph\\Generated\\Models\\EmailAddress;", result);
+        Assert.Contains("use Microsoft\\Graph\\Generated\\Models\\Extension;", result);
+        Assert.Contains("use Microsoft\\Graph\\Generated\\Models\\OpenTypeExtension;", result);
+        
+    }
 
     [Fact]
     public async Task GenerateForMoreComplexMapping()
